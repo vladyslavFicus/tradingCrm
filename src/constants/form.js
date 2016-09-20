3be8c4a946c02eb2802e166ac7ef4e9ebd66511e
intpp <intpplus@gmail.com>
2016-09-20 17:51:44 +0300

    Campaign manage updates

---
+++ src/constants/form.js
@@ -0,0 +1,25 @@
+const eventsTypes = {
+  FirstDeposit: 'First deposit',
+};
+
+const priorityMoneyTypeUsage = {
+  REAL: 'Real money',
+  BONUS: 'Bonus money',
+};
+
+const customValueFieldTypes = {
+  ABSOLUTE: 'ABSOLUTE',
+  PERCENTAGE: 'PERCENTAGE',
+};
+
+const customValueFieldTypesLabels = {
+  [customValueFieldTypes.ABSOLUTE]: 'Absolute',
+  [customValueFieldTypes.PERCENTAGE]: 'Percentage',
+};
+
+export {
+  eventsTypes,
+  priorityMoneyTypeUsage,
+  customValueFieldTypes,
+  customValueFieldTypesLabels,
+};
