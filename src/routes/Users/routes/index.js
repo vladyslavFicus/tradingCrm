48fa936d5058feed4ff35f4c5ee5b836f2110185
VS <slisarenko.vlad@gmail.com>
2016-12-13 12:55:43 +0200

    rename to LimitRoutes

--- src/routes/Users/routes/index.js
+++ src/routes/Users/routes/index.js
@@ -2,12 +2,12 @@ import ProfileRoute from './Profile';
 import GameActivityRoute from './GameActivity';
 import TransactionsRoute from './Transactions';
 import BonusesRoute from './Bonuses';
-import Limits from './Limits';
+import LimitsRoute from './Limits';
 
 export default (store) => [
   ProfileRoute(store),
   GameActivityRoute(store),
   TransactionsRoute(store),
   BonusesRoute(store),
-  Limits(store),
+  LimitsRoute(store),
 ];
