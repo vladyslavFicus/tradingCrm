2751a2060e1d44422610545e960c5d2e9a31a2ee
intpp <intpplus@gmail.com>
2017-01-31 11:53:09 +0200

    fix config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-config=$(curl -s $CONFIG_SERVICE_ROOT/backoffice/$BUILD_ENV  | jq -a -r -S 'reduce .propertySources[].source as $item ({}; . + $item)' | cat)
+config=$(curl -s $CONFIG_SERVICE_ROOT/backoffice/$BUILD_ENV \
+    --connect-timeout 5 \
+    --max-time 10 \
+    --retry 5 \
+    --retry-delay 0 \
+    --retry-max-time 60 | jq -a -r -S 'reduce .propertySources[].source as $item ({}; . + $item)' | cat)
 
 if ! [ -z $1 ]
 then
