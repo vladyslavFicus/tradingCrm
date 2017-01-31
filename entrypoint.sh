8fec3c5a1b8cb1bf6c4698a0040aa232fe7cec41
intpp <intpplus@gmail.com>
2017-01-31 14:32:53 +0200

    config retry

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 set -e
 
-config=$(curl -s $CONFIG_SERVICE_ROOT/backoffice/$BUILD_ENV \
-    --connect-timeout 5 \
-    --max-time 10 \
-    --retry 5 \
-    --retry-delay 0 \
-    --retry-max-time 60 | jq -a -r -S 'reduce .propertySources[].source as $item ({}; . + $item)' | cat)
+retryNumber=1
+retry=10
+sleepTime=5
+responseCode=0
+CONFIG_PATH="$CONFIG_SERVICE_ROOT/backoffice/$BUILD_ENV"
+
+while [ "$retryNumber" -le "$retry" ]; do
+  echo "Retry: $retryNumber, Sleep: $sleepTime"
+  echo "Fetching config status: #$retryNumber"
+  responseCode=$(curl -s -o /dev/null -w "%{http_code}" ${CONFIG_PATH} | cat)
+  echo "Response code: $responseCode"
+  ((retryNumber+=1))
+  ((sleepTime+=5))
+  if [ "$responseCode" -ne "200" ]
+  then
+    sleep $sleepTime
+  else
+    break
+  fi
+done
+
+config=$(curl -s $CONFIG_PATH | jq -a -r -S 'reduce .propertySources[].source as $item ({}; . + $item)' | cat)
 
 if [ -z "$config" ]; then
-  config="{}"
+  echo "Stopping service because service cannot fetch config." >&2
+  exit 1
 fi
 
 if ! [ -z $1 ]
