a0c1db827f2f821bd52c67c44b8d838ac12bed43
Pavel Evsegneev <pe@newagesol.com>
2017-03-10 18:55:00 +0200

    CP-814 Add transaction info popup

--- src/routes/UserProfile/routes/Transactions/container/ViewContainer.js
+++ src/routes/UserProfile/routes/Transactions/container/ViewContainer.js
@@ -4,10 +4,28 @@ import { connect } from 'react-redux';
 import { actionCreators as viewActionCreators } from '../modules/view';
 import { actionCreators as paymentActionCreators } from 'redux/modules/payment';
 
-const mapStateToProps = ({ userTransactions, profile: { bonus, view: { profile } } }) => ({
-  ...userTransactions,
-  currencyCode: profile.data.currencyCode,
-});
+const mapStateToProps = ({ userTransactions, profile: { bonus, view: { profile } } }) => {
+
+  const userBalance = profile.data.balance;
+
+  const emptyBalance = {
+    amount: 0,
+    currency: userBalance.currency,
+  };
+
+  return {
+    ...userTransactions,
+    currencyCode: profile.data.currencyCode,
+    profile: profile.data,
+    accumulatedBalances: {
+      bonus: bonus && bonus.data ? bonus.data.balance : emptyBalance,
+      real: bonus && bonus.data ? {
+          amount: userBalance.amount - bonus.data.balance.amount,
+          currency: userBalance.currency,
+        } : userBalance,
+    }
+  }
+};
 const mapActions = {
   ...viewActionCreators,
   onChangePaymentStatus: paymentActionCreators.changePaymentStatus,
