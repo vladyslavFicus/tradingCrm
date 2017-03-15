b8a0a42fb82fc6d5a4613cae7dacbddfd0470f9b
intpp <intpplus@gmail.com>
2017-03-15 15:47:04 +0200

    CP-807 - [BO] Operator Creation

--- src/components/ReduxForm/InputField.js
+++ src/components/ReduxForm/InputField.js
@@ -3,16 +3,25 @@ import classNames from 'classnames';
 
 class InputField extends Component {
   static propTypes = {
-    input: PropTypes.object.isRequired,
+    input: PropTypes.shape({
+      name: PropTypes.string,
+      value: PropTypes.any,
+    }).isRequired,
     label: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     position: PropTypes.oneOf(['horizontal', 'vertical']),
     showErrorMessage: PropTypes.bool,
+    disabled: PropTypes.bool,
+    meta: PropTypes.shape({
+      touched: PropTypes.bool,
+      error: PropTypes.string,
+    }).isRequired,
   };
 
   static defaultProps = {
     position: 'horizontal',
     showErrorMessage: true,
+    disabled: false,
   };
 
   render() {
@@ -24,13 +33,39 @@ class InputField extends Component {
   renderHorizontal = (props) => {
     const { input, label, type, disabled, meta: { touched, error }, showErrorMessage } = props;
 
-    return <div className={classNames('form-group row', { 'has-danger': touched && error })}>
-      <div className="col-md-3">
-        <label className="form-control-label">
-          {label}
-        </label>
+    return (
+      <div className={classNames('form-group row', { 'has-danger': touched && error })}>
+        <div className="col-md-3">
+          <label className="form-control-label">
+            {label}
+          </label>
+        </div>
+        <div className="col-md-9">
+          <input
+            {...input}
+            disabled={disabled}
+            type={type}
+            className={classNames('form-control', { 'has-danger': touched && error })}
+            placeholder={label}
+          />
+          {
+            showErrorMessage && touched && error &&
+            <div className="form-control-feedback">
+              {error}
+            </div>
+          }
+        </div>
       </div>
-      <div className="col-md-9">
+    );
+  };
+
+  renderVertical = (props) => {
+    const { input, label, type, disabled, meta: { touched, error }, showErrorMessage } = props;
+
+    return (
+      <div className={classNames('form-group', { 'has-danger': touched && error })}>
+        <label className="form-control-label">{label}</label>
+
         <input
           {...input}
           disabled={disabled}
@@ -45,29 +80,7 @@ class InputField extends Component {
           </div>
         }
       </div>
-    </div>;
-  };
-
-  renderVertical = (props) => {
-    const { input, label, type, disabled, meta: { touched, error }, showErrorMessage } = props;
-
-    return <div className={classNames('form-group', { 'has-danger': touched && error })}>
-      <label className="form-control-label">{label}</label>
-
-      <input
-        {...input}
-        disabled={disabled}
-        type={type}
-        className={classNames('form-control', { 'has-danger': touched && error })}
-        placeholder={label}
-      />
-      {
-        showErrorMessage && touched && error &&
-        <div className="form-control-feedback">
-          {error}
-        </div>
-      }
-    </div>;
+    );
   };
 }
 
--- src/components/ReduxForm/SelectField.js
+++ src/components/ReduxForm/SelectField.js
@@ -3,16 +3,27 @@ import classNames from 'classnames';
 
 class SelectField extends Component {
   static propTypes = {
-    input: PropTypes.object.isRequired,
+    input: PropTypes.shape({
+      name: PropTypes.string,
+      value: PropTypes.any,
+    }).isRequired,
     label: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
     position: PropTypes.oneOf(['horizontal', 'vertical']),
     showErrorMessage: PropTypes.bool,
+    multiple: PropTypes.bool,
+    disabled: PropTypes.bool,
+    meta: PropTypes.shape({
+      touched: PropTypes.bool,
+      error: PropTypes.string,
+    }).isRequired,
   };
 
   static defaultProps = {
     position: 'horizontal',
     showErrorMessage: true,
+    disabled: false,
+    multiple: false,
   };
 
   render() {
@@ -24,35 +35,9 @@ class SelectField extends Component {
   renderVertical = (props) => {
     const { input, label, children, multiple, disabled, meta: { touched, error }, showErrorMessage } = props;
 
-    return <div className={classNames('form-group row', { 'has-danger': touched && error })}>
-      <label className="form-control-label">{label}</label>
-      <select
-        {...input}
-        multiple={multiple}
-        disabled={disabled}
-        className={classNames('form-control', { 'has-danger': touched && error })}
-      >
-        {children}
-      </select>
-      {
-        showErrorMessage && touched && error &&
-        <div className="form-control-feedback">
-          {error}
-        </div>
-      }
-    </div>;
-  };
-
-  renderHorizontal = (props) => {
-    const { input, label, children, multiple, disabled, meta: { touched, error }, showErrorMessage } = props;
-
-    return <div className={classNames('form-group row', { 'has-danger': touched && error })}>
-      <div className="col-md-3">
-        <label className="form-control-label">
-          {label}
-        </label>
-      </div>
-      <div className="col-md-9">
+    return (
+      <div className={classNames('form-group row', { 'has-danger': touched && error })}>
+        <label className="form-control-label">{label}</label>
         <select
           {...input}
           multiple={multiple}
@@ -68,7 +53,37 @@ class SelectField extends Component {
           </div>
         }
       </div>
-    </div>;
+    );
+  };
+
+  renderHorizontal = (props) => {
+    const { input, label, children, multiple, disabled, meta: { touched, error }, showErrorMessage } = props;
+
+    return (
+      <div className={classNames('form-group row', { 'has-danger': touched && error })}>
+        <div className="col-md-3">
+          <label className="form-control-label">
+            {label}
+          </label>
+        </div>
+        <div className="col-md-9">
+          <select
+            {...input}
+            multiple={multiple}
+            disabled={disabled}
+            className={classNames('form-control', { 'has-danger': touched && error })}
+          >
+            {children}
+          </select>
+          {
+            showErrorMessage && touched && error &&
+            <div className="form-control-feedback">
+              {error}
+            </div>
+          }
+        </div>
+      </div>
+    );
   };
 }
 
