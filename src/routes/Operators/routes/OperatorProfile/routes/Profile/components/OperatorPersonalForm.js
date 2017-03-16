7f52d3a31ade06598f2a58b2f21fa4cbc6b70c67
Pavel Evsegneev <pe@newagesol.com>
2017-03-16 14:32:04 +0200

    CP-808 Add operator profile personal block

---
+++ src/routes/Operators/routes/OperatorProfile/routes/Profile/components/OperatorPersonalForm.js
@@ -0,0 +1,84 @@
+import React, { Component, PropTypes } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { InputField, SelectField } from 'components/ReduxForm/UserProfile';
+import { createValidator } from 'utils/validator';
+import countryList from 'country-list';
+
+const attributeLabels = {
+  firstName: 'First name',
+  lastName: 'Last name',
+  country: 'Country',
+};
+
+const countries = countryList().getData().reduce((result, item) => ({
+  ...result,
+  [item.code]: item.name,
+}), {});
+
+const validator = createValidator({
+  firstName: 'string',
+  lastName: 'string',
+  country: [`in:,${Object.keys(countries).join()}`],
+}, attributeLabels, false);
+
+class OperatorPersonalForm extends Component {
+  render() {
+    const {
+      handleSubmit,
+      onSubmit,
+      pristine,
+      submitting,
+      valid,
+    } = this.props;
+
+    return (
+      <div>
+        <form className="form-horizontal" role="form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="row">
+            <h5 className="pull-left">Personal information</h5>
+            { !(pristine || submitting || !valid) &&
+            <button className="btn btn-sm btn-primary pull-right" type="submit">
+              Save changes
+            </button>
+            }
+          </div>
+          <div className="row">
+            <Field
+              name="firstName"
+              label={attributeLabels.firstName}
+              type="text"
+              component={InputField}
+              wrapperClassName="col-lg-4"
+              showErrorMessage
+            />
+            <Field
+              name="lastName"
+              label={attributeLabels.lastName}
+              type="text"
+              component={InputField}
+              wrapperClassName="col-lg-5"
+              showErrorMessage
+            />
+            <Field
+              name="country"
+              label={attributeLabels.country}
+              type="text"
+              wrapperClassName="col-lg-4"
+              component={SelectField}
+            >
+              {Object
+                .keys(countries)
+                .map((key) => <option key={key} value={key}>{countries[key]}</option>)
+              }
+            </Field>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'updateOperatorProfilePersonal',
+  validate: validator,
+})(OperatorPersonalForm);
--- src/routes/Operators/routes/OperatorProfile/routes/Profile/components/View.js
+++ src/routes/Operators/routes/OperatorProfile/routes/Profile/components/View.js
@@ -1,10 +1,40 @@
 import React, { Component, PropTypes } from 'react';
+import OperatorPersonalForm from './OperatorPersonalForm';
 
 export default class OperatorProfile extends Component {
+
+  handleSubmitOperatorPersonal = (personalData) => {
+    console.log(personalData);
+  };
+
   render() {
-    console.log(this.props);
+    const {
+      data: {
+        firstName,
+        lastName,
+        country,
+      },
+    } = this.props;
+
     return (
-      <div>Operator profile {this.props.params.id}</div>
+      <div className="player__account__page_profile tab-content padding-vertical-20">
+        <div className="tab-pane active" id="home1" role="tabpanel">
+          <div className="panel">
+            <div className="panel-body row">
+              <div className="col-md-8">
+                <OperatorPersonalForm
+                  initialValues={{
+                    firstName,
+                    lastName,
+                    country,
+                  }}
+                  onSubmit={this.handleSubmitOperatorPersonal}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
@@ -13,4 +43,5 @@ OperatorProfile.propTypes = {
   params: PropTypes.shape({
     id: PropTypes.string,
   }),
+  data: PropTypes.object,
 };
