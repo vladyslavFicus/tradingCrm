eb9a7e43f73bb0d6dbc4b16c2b190082c6d1f214
Pavel Evsegneev <pe@newagesol.com>
2017-03-16 13:55:04 +0200

    CP-808 Add operator profile information block

--- src/routes/Operators/routes/OperatorProfile/layouts/OperatorProfileLayout.js
+++ src/routes/Operators/routes/OperatorProfile/layouts/OperatorProfileLayout.js
@@ -1,9 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 import Tabs from '../../../../../components/Tabs';
+import Information from '../../../../../components/Information/Container';
 import { operatorProfileTabs } from 'config/menu';
 import Header from '../components/Header';
+import "./OperatorProfileLayout.scss";
 
 export default class OperatorProfileLayout extends Component {
+
+  state = {
+    informationShown: true,
+  };
+
+  handleToggleInformationBlock = () => {
+    this.setState({ informationShown: !this.state.informationShown });
+  };
+
   render() {
     console.log(this.props);
 
@@ -11,16 +22,49 @@ export default class OperatorProfileLayout extends Component {
       location,
       params,
       children,
+      data,
     } = this.props;
 
+    const {
+      informationShown,
+    } = this.state;
+
+    const ip = {
+      entities: {
+        content: [],
+      },
+    };
+
     return (
-      <div className="player container panel profile-layout">
+      <div className="player container panel operator-profile-layout">
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-12">
-              <Header operatorProfile={this.props.data} />
+              <Header operatorProfile={data} />
             </div>
           </div>
+
+          <div className="row">
+            <div className="col-sm-12 operator-profile-layout-info-toggle">
+              <button className="operator-profile-layout-info-toggle-button" onClick={this.handleToggleInformationBlock}>
+                {!informationShown ? 'Show details' : 'Hide details'}
+              </button>
+              <div className="col-xs-12">
+                <hr />
+              </div>
+            </div>
+          </div>
+
+          {
+            informationShown &&
+            <Information
+              data={data}
+              ips={ip.entities.content}
+              updateSubscription={() => {} /* updateSubscription.bind(null, params.id) */}
+              showNotes={false}
+            />
+          }
+
           <div className="row">
             <section className="panel profile-user-content">
               <div className="panel-body">
