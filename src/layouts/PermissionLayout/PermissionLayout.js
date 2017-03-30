b64c1f5f1d443979d1857634737523f51c0cc8bf
VS <slisarenko.vlad@gmail.com>
2017-03-30 18:51:36 +0300

    propTypes fix

--- src/layouts/PermissionLayout/PermissionLayout.js
+++ src/layouts/PermissionLayout/PermissionLayout.js
@@ -2,6 +2,11 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 class PermissionLayout extends Component {
+  static propTypes = {
+    children: PropTypes.element.isRequired,
+    permissions: PropTypes.array,
+  };
+
   static childContextTypes = {
     permissions: PropTypes.arrayOf(PropTypes.string),
   };
@@ -24,11 +29,6 @@ class PermissionLayout extends Component {
     );
   }
 }
-
-PermissionLayout.propTypes = {
-  children: PropTypes.element.isRequired,
-  permissions: PropTypes.array,
-};
 
 const mapStateToProps = state => ({
   permissions: state.permissions.data,
