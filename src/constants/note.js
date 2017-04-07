32aed3a07e49b539a97a1fd662657977b40499c0
VS <slisarenko.vlad@gmail.com>
2017-04-07 14:05:40 +0300

    fetch note types

--- src/constants/note.js
+++ src/constants/note.js
@@ -1,11 +1,22 @@
-const targetTypes = {
-  PROFILE: 'profile',
-  BONUS: 'bonus',
-  PAYMENT: 'payment',
-  FILE: 'file',
-  LIMIT: 'limit',
+import keyMirror from 'keymirror';
+
+const targetTypes = keyMirror({
+  PROFILE: null,
+  BONUS: null,
+  PAYMENT: null,
+  FILE: null,
+  LIMIT: null,
+});
+
+const targetTypesLabels = {
+  [targetTypes.PROFILE]: 'Profile',
+  [targetTypes.BONUS]: 'Bonus',
+  [targetTypes.PAYMENT]: 'Payment',
+  [targetTypes.FILE]: 'File',
+  [targetTypes.LIMIT]: 'Limit',
 };
 
 export {
   targetTypes,
+  targetTypesLabels,
 };
--- src/routes/UserProfile/routes/Notes/modules/index.js
+++ src/routes/UserProfile/routes/Notes/modules/index.js
@@ -6,14 +6,23 @@ import view, {
   initialState as viewInitialState,
 } from './view';
 
+import noteTypes, {
+  actionCreators as noteTypesActionCreators,
+  actionTypes as noteTypesActionTypes,
+  initialState as noteTypesInitialState,
+} from './noteTypes';
+
 const actionCreators = {
   ...viewActionCreators,
+  ...noteTypesActionCreators,
 };
 const actionTypes = {
   ...viewActionTypes,
+  ...noteTypesActionTypes,
 };
 const initialState = {
   view: viewInitialState,
+  noteTypes: noteTypesInitialState,
 };
 
 export {
@@ -24,4 +33,5 @@ export {
 
 export default combineReducers({
   view,
+  noteTypes,
 });
