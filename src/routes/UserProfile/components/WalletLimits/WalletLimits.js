5bcb50c865c08a2831e3ea170ab5344307f2e961
KostyaDyachenko <kd@newagesol.com>
2017-04-10 18:51:00 +0200

    WalletLimits finished.

--- src/routes/UserProfile/components/WalletLimits/WalletLimits.js
+++ src/routes/UserProfile/components/WalletLimits/WalletLimits.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Dropdown, DropdownMenu } from 'reactstrap';
 import classNames from 'classnames';
 import PropTypes from '../../../../constants/propTypes';
 import './WalletLimits.scss';
@@ -24,9 +24,47 @@ class WalletLimits extends Component {
       {label}
 
       <DropdownMenu>
-        <button type="button" className="btn btn-danger-outline margin-right-10">Unlock deposit</button>
-        <button type="button" className="btn btn-danger-outline">Lock Withdrawal</button>
-        <div className="text-uppercase font-size-11">Withdraws</div>
+        <div className="header-block_wallet-limits_btn-group">
+            <button type="button" className="btn btn-danger-outline margin-right-10">Unlock deposit</button>
+            <button type="button" className="btn btn-danger-outline">Lock withdrawal</button>
+        </div>
+        <div className="limits-info">
+          <div className="limits-info_tab">
+            <div className="header-block_wallet-limits-tab_status">
+              Deposit - <span className="header-block_wallet-limits-tab_status_is-locked">Locked</span>
+            </div>
+            <div className="header-block_wallet-limits-tab_log">by OP-675p567</div>
+            <div className="header-block_wallet-limits-tab_log">Reason - Reason3</div>
+            <div className="header-block_wallet-limits-tab_log">on 14.10.2016 13:00</div>
+          </div>
+          <div className="limits-info_tab">
+            <div className="header-block_wallet-limits-tab_status">
+              Deposit - <span className="header-block_wallet-limits-tab_status_is-locked">Locked</span>
+            </div>
+            <div className="header-block_wallet-limits-tab_log">Reason: Profile incomplete</div>
+          </div>
+          <div className="limits-info_tab">
+            <div className="header-block_wallet-limits-tab_status">
+              Withdrawal - <span className="header-block_wallet-limits-tab_status_is-locked">Locked</span>
+            </div>
+            <div className="header-block_wallet-limits-tab_log">by OP-675p567</div>
+            <div className="header-block_wallet-limits-tab_log">Reason - Reason4</div>
+            <div className="header-block_wallet-limits-tab_log">on 14.10.2016 13:00</div>
+          </div>
+          <div className="limits-info_tab">
+            <div className="header-block_wallet-limits-tab_status">
+              Withdrawal - <span className="header-block_wallet-limits-tab_status_is-locked">Locked</span>
+            </div>
+            <div className="header-block_wallet-limits-tab_log">Reason - KYC not verified</div>
+          </div>
+          <div className="limits-info_tab">
+            <div className="header-block_wallet-limits-tab_status">
+              Withdrawal - <span className="header-block_wallet-limits-tab_status_is-locked">Locked</span>
+            </div>
+            <div className="header-block_wallet-limits-tab_log">by BM-675p567</div>
+            <div className="header-block_wallet-limits-tab_log">until 14.10.2016 13:00</div>
+          </div>
+        </div>
       </DropdownMenu>
     </Dropdown>
   );
