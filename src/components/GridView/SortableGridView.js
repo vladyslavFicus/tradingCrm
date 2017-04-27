20d7ec5189e35c601490f171528d0dedeed4ee2b
VS <slisarenko.vlad@gmail.com>
2017-04-27 18:59:37 +0300

    hotfix

--- src/components/GridView/SortableGridView.js
+++ src/components/GridView/SortableGridView.js
@@ -29,7 +29,7 @@ const SortableList = SortableContainer(({ items, columns }) => {
   return (
     <tbody>
       {items.map((value, index) => (
-        <SortableItem key={`item-${index}`} index={index + 1} data={value} columns={columns} />
+        <SortableItem key={`item-${index}`} index={index} data={value} columns={columns} />
       ))}
     </tbody>
   );
@@ -71,7 +71,7 @@ class SortableGridView extends Component {
   onSortEnd = ({ oldIndex, newIndex }) => {
     this.setState({
       dataSource: arrayMove(this.state.dataSource, oldIndex, newIndex),
-    }, this.props.onSortEnd({ from: oldIndex, to: newIndex }));
+    }, () => this.props.onSortEnd({ from: oldIndex + 1, to: newIndex + 1}));
   };
 
   recognizeHeaders = (grids) => {
