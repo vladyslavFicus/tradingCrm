3f2495fbff14d746c01a971538b57e8f5f720016
KostyaDyachenko <kd@newagesol.com>
2017-05-10 18:51:45 +0200

    Started adding new custom-scrollbar.
    Added dependency.
    
    Need to finish: component! and hide onMouseOver.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Nav from '../Nav';
 import PropTypes from '../../constants/propTypes';
+import Scrollbars from 'react-custom-scrollbars';
 import './Sidebar.scss';
 
 class Sidebar extends Component {
@@ -9,12 +10,40 @@ class Sidebar extends Component {
     bottomMenu: PropTypes.arrayOf(PropTypes.navItem).isRequired,
   };
 
-  render() {
+  state = {
+    scrollbarHover: false,
+  };
+
+  handleMouseEnter = () => {
+    this.setState({ scrollbarHover: true });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ scrollbarHover: false });
+  };
+
+  renderThumbVertical = ({ style, ...props }) => {
+    const { hover } = this.state;
+    return <div style={{ ...style, opacity: hover ? 1 : 0 }} {...props} />;
+  };
+
+  render () {
     return (
       <aside className="sidebar">
-        <Nav
-          items={this.props.topMenu}
-        />
+        <Scrollbars
+          renderThumbVertical={this.renderTrackVertical}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+          renderTrackHorizontal={props => <div {...props} className="track-vertical" style={{ display: 'none' }} />}
+          renderThumbHorizontal={props => <div {...props} className="thumb-vertical" style={{ display: 'none' }} />}
+          style={{
+            height: 'calc(100% - 100px)',
+          }}
+        >
+          <Nav
+            items={this.props.topMenu}
+          />
+        </Scrollbars>
         <Nav
           className="navbar-nav support-tab"
           items={this.props.bottomMenu}
