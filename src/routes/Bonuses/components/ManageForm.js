6f5724c1d50202913e8a341ff535bc78af5cfdc2
intpp <intpplus@gmail.com>
2017-05-12 12:34:06 +0300

    Manual bonus modal

--- src/routes/Bonuses/components/ManageForm.js
+++ src/routes/Bonuses/components/ManageForm.js
@@ -43,8 +43,19 @@ const validator = createValidator({
 }, attributeLabels, false);
 
 class ManageForm extends Component {
-  handleResetForm = () => {
-    this.props.reset();
+  static propTypes = {
+    handleSubmit: PropTypes.func,
+    reset: PropTypes.func,
+    pristine: PropTypes.bool,
+    submitting: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onMount: PropTypes.func,
+    onSubmit: PropTypes.func.isRequired,
+    currencies: PropTypes.array,
+  };
+  static defaultProps = {
+    currencies: [],
+    onMount: null,
   };
 
   componentDidMount() {
@@ -53,137 +64,135 @@ class ManageForm extends Component {
     }
   }
 
+  handleResetForm = () => {
+    this.props.reset();
+  };
+
   render() {
     const { handleSubmit, pristine, submitting, onSubmit, disabled, currencies } = this.props;
 
-    return <form onSubmit={handleSubmit(onSubmit)}>
-      {disabled && <div className="alert alert-warning">You can't edit the bonus.</div>}
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {disabled && <div className="alert alert-warning">You can't edit the bonus.</div>}
 
-      <Field
-        name="label"
-        label={attributeLabels.label}
-        type="text"
-        disabled={disabled}
-        component={InputField}
-      />
-      <Field
-        name="priority"
-        label={attributeLabels.priority}
-        type="text"
-        disabled={disabled}
-        component={InputField}
-      />
-      <Field
-        name="playerUUID"
-        label={attributeLabels.playerUUID}
-        type="text"
-        disabled={disabled}
-        component={InputField}
-      />
-      <Field
-        name="currency"
-        label={attributeLabels.currency}
-        type="select"
-        disabled={disabled}
-        component={SelectField}
-      >
-        <option value="">--- Chose currency ---</option>
-        {currencies.map((item) => (
-          <option key={item} value={item}>
-            {item}
-          </option>
-        ))}
-      </Field>
-      <Field
-        name="grantedAmount"
-        label={attributeLabels.grantedAmount}
-        type="text"
-        disabled={disabled}
-        component={InputField}
-      />
-      <Field
-        name="amountToWage"
-        label={attributeLabels.amountToWage}
-        type="text"
-        disabled={disabled}
-        component={InputField}
-      />
-      <Field
-        name="expirationDate"
-        label={attributeLabels.expirationDate}
-        disabled={disabled}
-        component={SingleDateField}
-        onChange={this.props.change}
-      />
-      <Field
-        name="prize"
-        label={attributeLabels.prize}
-        type="text"
-        disabled={disabled}
-        component={InputField}
-      />
-      <Field
-        name="capping"
-        label={attributeLabels.capping}
-        type="text"
-        disabled={disabled}
-        component={InputField}
-      />
+        <Field
+          name="label"
+          label={attributeLabels.label}
+          type="text"
+          disabled={disabled}
+          component={InputField}
+        />
+        <Field
+          name="priority"
+          label={attributeLabels.priority}
+          type="text"
+          disabled={disabled}
+          component={InputField}
+        />
+        <Field
+          name="playerUUID"
+          label={attributeLabels.playerUUID}
+          type="text"
+          disabled={disabled}
+          component={InputField}
+        />
+        <Field
+          name="currency"
+          label={attributeLabels.currency}
+          type="select"
+          disabled={disabled}
+          component={SelectField}
+        >
+          <option value="">--- Chose currency ---</option>
+          {currencies.map(item => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </Field>
+        <Field
+          name="grantedAmount"
+          label={attributeLabels.grantedAmount}
+          type="text"
+          disabled={disabled}
+          component={InputField}
+        />
+        <Field
+          name="amountToWage"
+          label={attributeLabels.amountToWage}
+          type="text"
+          disabled={disabled}
+          component={InputField}
+        />
+        <Field
+          name="expirationDate"
+          label={attributeLabels.expirationDate}
+          disabled={disabled}
+          component={SingleDateField}
+          onChange={this.props.change}
+        />
+        <Field
+          name="prize"
+          label={attributeLabels.prize}
+          type="text"
+          disabled={disabled}
+          component={InputField}
+        />
+        <Field
+          name="capping"
+          label={attributeLabels.capping}
+          type="text"
+          disabled={disabled}
+          component={InputField}
+        />
 
-      <div className={classNames('form-group row')}>
-        <div className="col-md-9 col-md-offset-3">
-          <div className="checkbox">
-            <label>
-              <Field
-                name="optIn"
-                type="checkbox"
-                component="input"
-                disabled={disabled}
-              /> {attributeLabels.optIn}
-            </label>
-          </div>
-        </div>
-      </div>
-
-      {!disabled && <div className="form-actions">
-        <div className="form-group row">
+        <div className={classNames('form-group row')}>
           <div className="col-md-9 col-md-offset-3">
-            <button
-              type="submit"
-              disabled={submitting}
-              className="btn width-150 btn-primary"
-            >
-              Submit
-            </button>
-
-            <button
-              type="button"
-              disabled={pristine || submitting}
-              onClick={this.handleResetForm}
-              className="btn btn-default"
-            >
-              Cancel
-            </button>
+            <div className="checkbox">
+              <label>
+                <Field
+                  name="optIn"
+                  type="checkbox"
+                  component="input"
+                  disabled={disabled}
+                /> {attributeLabels.optIn}
+              </label>
+            </div>
           </div>
         </div>
-      </div>}
-    </form>;
+
+        {!disabled && <div className="form-actions">
+          <div className="form-group row">
+            <div className="col-md-9 col-md-offset-3">
+              <button
+                type="submit"
+                disabled={submitting}
+                className="btn width-150 btn-primary"
+              >
+                Submit
+              </button>
+
+              <button
+                type="button"
+                disabled={pristine || submitting}
+                onClick={this.handleResetForm}
+                className="btn btn-default"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>}
+      </form>
+    );
   }
 }
-
-ManageForm.defaultProps = {
-  currencies: [],
-};
-ManageForm.propTypes = {
-  onMount: PropTypes.func,
-  currencies: PropTypes.array,
-};
 
 let ManageReduxForm = reduxForm({
   form: formName,
   validate: validator,
 })(ManageForm);
-ManageReduxForm = connect((state) => ({
+ManageReduxForm = connect(state => ({
   errors: errorSelector(state),
 }), {})(ManageReduxForm);
 
--- src/routes/UserProfile/routes/Bonuses/components/ViewModal.js
+++ src/routes/UserProfile/routes/Bonuses/components/ViewModal.js
@@ -33,8 +33,8 @@ class ViewModal extends Component {
             actions.length === 2 &&
             <div className="row">
               {actions.map((action, index) => (
-                <div key={index} className={classNames("col-md-6", { 'text-right': index !== 0 })}>
-                  <button {...action}/>
+                <div key={index} className={classNames('col-md-6', { 'text-right': index !== 0 })}>
+                  <button {...action} />
                 </div>
               ))}
             </div>
@@ -44,46 +44,40 @@ class ViewModal extends Component {
     );
   }
 
-  renderBonusStats = (data) => {
-    return <div className="row well player-header-blocks">
-      <div className="col-md-3 grey-back-tab">
-        <div className="color-default text-uppercase font-size-11">
+  renderBonusStats = data => <div className="row well player-header-blocks">
+    <div className="col-md-3 grey-back-tab">
+      <div className="color-default text-uppercase font-size-11">
           Granted
         </div>
 
-        {this.renderGrantedAmount(data)}
-      </div>
-      <div className="col-md-3 grey-back-tab">
-        <div className="color-default text-uppercase font-size-11">
+      {this.renderGrantedAmount(data)}
+    </div>
+    <div className="col-md-3 grey-back-tab">
+      <div className="color-default text-uppercase font-size-11">
           Wagered
         </div>
 
-        {this.renderWageredAmount(data)}
-      </div>
-      <div className="col-md-3 grey-back-tab">
-        <div className="color-default text-uppercase font-size-11">
+      {this.renderWageredAmount(data)}
+    </div>
+    <div className="col-md-3 grey-back-tab">
+      <div className="color-default text-uppercase font-size-11">
           To wager
         </div>
 
-        {this.renderToWagerAmount(data)}
-      </div>
-      <div className="col-md-3 grey-back-tab">
-        <div className="color-default text-uppercase font-size-11">
+      {this.renderToWagerAmount(data)}
+    </div>
+    <div className="col-md-3 grey-back-tab">
+      <div className="color-default text-uppercase font-size-11">
           Total to wager
         </div>
 
-        {this.renderTotalToWagerAmount(data)}
-      </div>
-    </div>;
-  };
+      {this.renderTotalToWagerAmount(data)}
+    </div>
+  </div>;
 
-  renderGrantedAmount = (data) => {
-    return <Amount className="font-weight-600 font-size-20 color-primary" {...data.grantedAmount}/>;
-  };
+  renderGrantedAmount = data => <Amount className="font-weight-600 font-size-20 color-primary" {...data.grantedAmount} />;
 
-  renderWageredAmount = (data) => {
-    return <Amount className="font-weight-600 font-size-20 color-primary" {...data.wagered}/>;
-  };
+  renderWageredAmount = data => <Amount className="font-weight-600 font-size-20 color-primary" {...data.wagered} />;
 
   renderToWagerAmount = (data) => {
     const toWagerAmount = {
@@ -92,153 +86,137 @@ class ViewModal extends Component {
         data.wagered && !isNaN(data.wagered.amount)
           ? data.amountToWage.amount - data.wagered.amount : 0,
         0
-      ), currency: data.currency
+      ),
+      currency: data.currency,
     };
 
-    return <Amount className="font-weight-600 font-size-20 color-primary" {...toWagerAmount}/>;
+    return <Amount className="font-weight-600 font-size-20 color-primary" {...toWagerAmount} />;
   };
 
-  renderTotalToWagerAmount = (data) => {
-    return <Amount className="font-weight-600 font-size-20 color-primary" {...data.amountToWage}/>;
-  };
+  renderTotalToWagerAmount = data => <Amount className="font-weight-600 font-size-20 color-primary" {...data.amountToWage} />;
 
-  renderBonus = (item) => {
-    return <div className="row margin-vertical-20">
-      <div className="col-md-3">
-        <div className="color-default text-uppercase font-size-11">
+  renderBonus = item => <div className="row margin-vertical-20">
+    <div className="col-md-3">
+      <div className="color-default text-uppercase font-size-11">
           Bonus
         </div>
 
-        {this.renderMainInfo(item)}
-      </div>
-      <div className="col-md-3">
-        <div className="color-default text-uppercase font-size-11">
+      {this.renderMainInfo(item)}
+    </div>
+    <div className="col-md-3">
+      <div className="color-default text-uppercase font-size-11">
           Available
         </div>
 
-        {this.renderAvailablePeriod(item)}
-      </div>
-      <div className="col-md-2">
-        <div className="color-default text-uppercase font-size-11">
+      {this.renderAvailablePeriod(item)}
+    </div>
+    <div className="col-md-2">
+      <div className="color-default text-uppercase font-size-11">
           Priority
         </div>
 
-        {this.renderPriority(item)}
-      </div>
-      <BonusType className="col-md-2" bonus={item} label="Bonus type"/>
-      <BonusStatus className="col-md-2" bonus={item} label="Status"/>
-    </div>;
-  };
+      {this.renderPriority(item)}
+    </div>
+    <BonusType className="col-md-2" bonus={item} label="Bonus type" />
+    <BonusStatus className="col-md-2" bonus={item} label="Status" />
+  </div>;
 
-  renderMainInfo = (data) => {
-    return <span>
-      <div className="font-weight-600">{data.label}</div>
-      <div className="little-grey-text font-size-11">{shortify(data.bonusUUID, 'BM')}</div>
-      {
+  renderMainInfo = data => <span>
+    <div className="font-weight-600">{data.label}</div>
+    <div className="little-grey-text font-size-11">{shortify(data.bonusUUID, 'BM')}</div>
+    {
         !!data.campaignUUID &&
         <div className="little-grey-text font-size-11">
           by Campaign {shortify(data.campaignUUID, 'CO')}
         </div>
       }
-      {
+    {
         !data.campaignUUID && !!data.operatorUUID &&
         <div className="ittle-grey-text font-size-11">
           by Manual Bonus {shortify(data.operatorUUID, 'OP')}
         </div>
       }
-    </span>;
-  };
+  </span>;
 
-  renderAvailablePeriod = (data) => {
-    return data.createdDate ? <div>
-      <div className="font-weight-600">
-        {moment(data.createdDate).format('DD.MM.YYYY HH:mm:ss')}
-        </div>
-      {
+  renderAvailablePeriod = data => data.createdDate ? <div>
+    <div className="font-weight-600">
+      {moment(data.createdDate).format('DD.MM.YYYY HH:mm:ss')}
+    </div>
+    {
         !!data.expirationDate &&
         <div className="little-grey-text font-size-11">
           {moment(data.expirationDate).format('DD.MM.YYYY HH:mm:ss')}
         </div>
       }
-    </div> : <span>&mdash</span>;
-  };
+  </div> : <span>&mdash</span>;
 
-  renderPriority = (data) => {
-    return <span>{data.priority}</span>;
-  };
+  renderPriority = data => <span>{data.priority}</span>;
 
-  renderPlayer = (profile, balances) => {
-    return <div className="row player-header-blocks margin-bottom-10 equal">
-      <div className="col-sm-4 equal-in">
-        <div className="color-default text-uppercase font-size-11">
+  renderPlayer = (profile, balances) => <div className="row player-header-blocks margin-bottom-10 equal">
+    <div className="col-sm-4 equal-in">
+      <div className="color-default text-uppercase font-size-11">
           Player
         </div>
 
-        {this.renderPlayerInfo(profile)}
-      </div>
-      <div className="col-sm-4 equal-in">
-        <div className="color-default text-uppercase font-size-11">
+      {this.renderPlayerInfo(profile)}
+    </div>
+    <div className="col-sm-4 equal-in">
+      <div className="color-default text-uppercase font-size-11">
           Account status
         </div>
 
-        {this.renderPlayerStatus(profile)}
-      </div>
-      <div className="col-sm-4 equal-in">
-        <div className="color-default text-uppercase font-size-11">
+      {this.renderPlayerStatus(profile)}
+    </div>
+    <div className="col-sm-4 equal-in">
+      <div className="color-default text-uppercase font-size-11">
           Balance
         </div>
 
-        {this.renderBalance(balances)}
-      </div>
-    </div>;
-  };
+      {this.renderBalance(balances)}
+    </div>
+  </div>;
 
   /**
    * @todo Move to component
    */
-  renderPlayerInfo = (profile) => {
-    return <div className="line-height-1">
-      <span
-        className="font-weight-600 text-capitalize font-size-14">
-        {[profile.firstName, profile.lastName].join(' ')}
-      </span>
-      {' '}
-      {!!profile.birthDate && <span>({moment().diff(profile.birthDate, 'years')})</span>}
-      <br/>
-      <span className="little-grey-text font-size-11">{profile.username} - {shortify(profile.uuid, 'PL')}</span>
-    </div>;
-  };
+  renderPlayerInfo = profile => <div className="line-height-1">
+    <span
+      className="font-weight-600 text-capitalize font-size-14"
+    >
+      {[profile.firstName, profile.lastName].join(' ')}
+    </span>
+    {' '}
+    {!!profile.birthDate && <span>({moment().diff(profile.birthDate, 'years')})</span>}
+    <br />
+    <span className="little-grey-text font-size-11">{profile.username} - {shortify(profile.uuid, 'PL')}</span>
+  </div>;
 
   /**
    * @todo Move to component
    */
-  renderPlayerStatus = (profile) => {
-    return <div>
-      <div
-        className={`font-weight-600 text-uppercase ${statusColorNames[profile.status]}`}
-      >
-        {profile.status}
-        </div>
-      {
+  renderPlayerStatus = profile => <div>
+    <div
+      className={`font-weight-600 text-uppercase ${statusColorNames[profile.status]}`}
+    >
+      {profile.status}
+    </div>
+    {
         !!profile.suspendEndDate &&
         <div className="color-default font-size-11">
           Until {moment(profile.suspendEndDate).format('L')}
         </div>
       }
-    </div>;
-  };
+  </div>;
 
   /**
    * @todo Move to component
    */
-  renderBalance = ({ total, bonus, real }) => {
-    return <div>
-      <Amount tag="div" className={'font-weight-600 text-uppercase'} {...total} />
-      <div className="little-grey-text font-size-11">
-        RM <Amount { ...real } /> + BM <Amount { ...bonus } />
-      </div>
-    </div>;
-  };
+  renderBalance = ({ total, bonus, real }) => <div>
+    <Amount tag="div" className={'font-weight-600 text-uppercase'} {...total} />
+    <div className="little-grey-text font-size-11">
+        RM <Amount {...real} /> + BM <Amount {...bonus} />
+    </div>
+  </div>;
 }
 
 export default ViewModal;
