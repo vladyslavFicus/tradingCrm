8d3c728d9f1e47d5f3dac6106aa4f43ccc49f1ba
intpp <intpplus@gmail.com>
2017-05-15 12:28:07 +0300

    Reset password | Access denied

--- src/routes/ResetPassword/index.js
+++ src/routes/ResetPassword/index.js
@@ -1,15 +1,9 @@
-export default store => ({
+export default () => ({
   path: 'reset-password',
   getComponent({ location: { query } }, cb) {
     require.ensure([], (require) => {
-      const { auth: { logged } } = store.getState();
-
       if (!query.token) {
         return cb(null, require('../../routes/NotFound/container/Container').default);
-      }
-
-      if (logged) {
-        return cb(null, require('../../routes/Forbidden/container/Container').default);
       }
 
       return cb(null, require('./container/Container').default);
