c2195aebcbb777276c8274f2103f948c3b788e2f
intpp <intpplus@gmail.com>
2017-05-15 11:53:40 +0300

    Ignore "JWT_TOKEN_EXPIRED" error

--- src/redux/middlewares/apiErrors.js
+++ src/redux/middlewares/apiErrors.js
@@ -1,40 +1,44 @@
 import { sendError, errorTypes } from '../../utils/errorLog';
+import { actionTypes as authActionTypes } from '../modules/auth';
 
 const regExp = new RegExp('-failure$');
+const ignoredActionTypes = [authActionTypes.VALIDATE_TOKEN.FAILURE];
 
 export default () => next => (action) => {
-  if (action && action.error && regExp.test(action.type)) {
-    const message = `${errorTypes.API} error`;
-    const errorType = errorTypes.API;
+  if (action && action.error) {
+    if (ignoredActionTypes.indexOf(action.type) === -1 && regExp.test(action.type)) {
+      const message = `${errorTypes.API} error`;
+      const errorType = errorTypes.API;
 
-    const error = {
-      errorType,
-      message,
-      response: {
-        error: errorType,
-        error_description: message,
-      },
-    };
+      const error = {
+        errorType,
+        message,
+        response: {
+          error: errorType,
+          error_description: message,
+        },
+      };
 
-    if (action.type) {
-      error.actionType = action.type;
+      if (action.type) {
+        error.actionType = action.type;
+      }
+
+      if (action.payload && action.payload.status) {
+        const errorCode = action.payload.status;
+        error.errorCode = errorCode;
+        error.response.error = errorCode;
+      }
+
+      if (action.payload && action.payload.response) {
+        error.response = action.payload.response;
+      }
+
+      if (action.payload && action.payload.response && action.payload.response.error) {
+        error.message = action.payload.response.error;
+      }
+
+      sendError(error);
     }
-
-    if (action.payload && action.payload.status) {
-      const errorCode = action.payload.status;
-      error.errorCode = errorCode;
-      error.response.error = errorCode;
-    }
-
-    if (action.payload && action.payload.response) {
-      error.response = action.payload.response;
-    }
-
-    if (action.payload && action.payload.response && action.payload.response.error) {
-      error.message = action.payload.response.error;
-    }
-
-    sendError(error);
   }
 
   return next(action);
