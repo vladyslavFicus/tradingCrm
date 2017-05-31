6c7a69ad3316a404447f57fafa47df9ebcf040da
VS <slisarenko.vlad@gmail.com>
2017-05-31 16:49:37 +0300

    use specific fingerprint params

--- src/utils/fingerPrint.js
+++ src/utils/fingerPrint.js
@@ -3,6 +3,12 @@ import Fingerprint2 from 'fingerprintjs2';
 export default () => new Promise((resolve) => {
   new Fingerprint2().get((hash, components) => resolve({
     hash,
-    params: components.reduce((result, item) => ({ ...result, [item.key]: [item.value] }), {}),
+    params: components
+      .filter(({ key }) => ([
+        'user_agent', 'language', 'color_depth',
+        'pixel_ratio', 'hardware_concurrency', 'resolution', 'available_resolution',
+        'timezone_offset', 'cpu_class', 'navigator_platform', 'do_not_track', 'regular_plugins',
+      ].indexOf(key) > -1))
+      .reduce((result, item) => ({ ...result, [item.key]: [item.value] }), {}),
   }));
 });
