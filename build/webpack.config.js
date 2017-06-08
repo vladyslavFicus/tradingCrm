34dc961a3d83f9a5e6ca316ede1368d0bc417c79
intpp <intpplus@gmail.com>
2017-06-08 17:21:40 +0300

    Fix webpack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -74,7 +74,7 @@ const config = {
             presets: [
               'babel-preset-react',
               ['babel-preset-env', {
-                modules: true,
+                modules: 'commonjs',
                 targets: {
                   ie9: true,
                 },
