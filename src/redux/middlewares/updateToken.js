0ead5fb42a5fb694b52a6f382dbc940c21ac0ca1
intpp <intpplus@gmail.com>
2017-06-22 12:36:02 +0300

    Logout by token expiration time

--- src/redux/middlewares/updateToken.js
+++ src/redux/middlewares/updateToken.js
@@ -1,5 +1,6 @@
 import { isValidRSAA } from 'redux-api-middleware';
 import jwtDecode from 'jwt-decode';
+import { browserHistory } from 'react-router';
 import {
   actionCreators as authActionCreators,
 } from '../../redux/modules/auth';
@@ -7,7 +8,24 @@ import timestamp from '../../utils/timestamp';
 
 const state = {
   pending: false,
+  logoutTimer: null,
 };
+
+function logout(store) {
+  const { auth: { logged, token } } = store.getState();
+
+  if (logged && token) {
+    const tokenData = jwtDecode(token);
+
+    if (tokenData.exp - timestamp() <= 0) {
+      clearTimeout(state.logoutTimer);
+      state.logoutTimer = null;
+
+      browserHistory.push('/logout');
+    }
+  }
+}
+
 export default function ({ expireThreshold = 60 }) {
   return store => next => async (action) => {
     if (!state.pending) {
@@ -15,17 +33,33 @@ export default function ({ expireThreshold = 60 }) {
 
       if (logged && token) {
         const tokenData = jwtDecode(token);
+        const time = timestamp();
 
-        if ((tokenData.exp - timestamp()) <= expireThreshold && isValidRSAA(action)) {
-          console.info('[Token]: Start refreshing...', tokenData.exp - timestamp(), expireThreshold);
+        if ((tokenData.exp - time) <= expireThreshold && isValidRSAA(action)) {
+          console.info('[Token]: Start refreshing...');
+          console.log(tokenData.exp - timestamp(), expireThreshold, time, tokenData.exp);
+
           state.pending = true;
 
           const responseAction = await next(action);
-          await store.dispatch(authActionCreators.refreshToken());
+          const refreshTokenAction = await store.dispatch(authActionCreators.refreshToken());
           console.info('[Token]: Stop refreshing...');
           state.pending = false;
 
+          if (refreshTokenAction && !refreshTokenAction.error) {
+            const refreshedTokenData = jwtDecode(refreshTokenAction.payload.jwtToken);
+            clearTimeout(state.logoutTimer);
+            state.logoutTimer = setTimeout(() => {
+              logout(store);
+            }, (refreshedTokenData.exp - (timestamp() + 1)) * 1000);
+          }
+
           return responseAction;
+        } else if (state.logoutTimer === null) {
+          console.log(tokenData.exp - time);
+          state.logoutTimer = setTimeout(() => {
+            logout(store);
+          }, (tokenData.exp - (timestamp() + 1)) * 1000);
         }
       }
     }
