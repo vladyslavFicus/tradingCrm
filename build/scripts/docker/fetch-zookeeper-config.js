d2d6529a5fa8c5ad676286743e7bf5d5717447ef
VS <slisarenko.vlad@gmail.com>
2017-06-26 10:20:56 +0300

    zookeeper config

--- build/scripts/docker/fetch-zookeeper-config.js
+++ build/scripts/docker/fetch-zookeeper-config.js
@@ -2,6 +2,10 @@
 const zookeeper = require('node-zookeeper-client');
 const _ = require('lodash');
 
+const getChildren = (client, configPath) => new Promise((resolve) => {
+  client.getChildren(configPath, (error, children) => resolve(children));
+});
+
 module.exports = function (params) {
   return new Promise(function (resolve, reject) {
     const environmentConfig = params.environmentConfig;
@@ -10,28 +14,29 @@ module.exports = function (params) {
       const configPath = `/system/${environmentConfig.brand.name}/nas/brand`;
       const client = zookeeper.createClient(environmentConfig.zookeeper.url);
 
-      client.once('connected', function () {
+      client.once('connected', async function () {
+        const childKeys = await getChildren(client, configPath);
+
         const config = {};
         Promise.all(
-          params.allowedKeys
-            .map(function (key) {
-              return new Promise(function (res) {
-                client.getData(`${configPath}/${key}`, function (err, data) {
-                  const value = data.toString('UTF-8');
+          childKeys.map(function (key) {
+            return new Promise(function (res) {
+              client.getData(`${configPath}/${key}`, function (err, data) {
+                const value = data.toString('UTF-8');
 
-                  if (err) {
-                    reject(err.stack);
+                if (err) {
+                  reject(err.stack);
 
-                    return;
-                  }
+                  return;
+                }
 
-                  _.set(config, key, value);
-                  res();
+                _.set(config, key, value);
+                res();
 
-                  return value;
-                });
+                return value;
               });
-            })
+            });
+          })
         ).then(function () {
           client.close();
 
