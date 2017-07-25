263332a499c3bdc5188a3dd2e5698bb4ba13491f
KostyaDyachenko <kd@newagesol.com>
2017-07-25 18:04:08 +0300

    commit

--- src/routes/Operators/routes/OperatorProfile/components/Information/Information.js
+++ src/routes/Operators/routes/OperatorProfile/components/Information/Information.js
@@ -7,15 +7,17 @@ import PropTypes from '../../../../../../constants/propTypes';
 import './Information.scss';
 
 const Information = ({ data, ips }) => (
-  <div className="player__account__details row">
-    <div className="col-md-4">
-      <Personal data={data} />
-    </div>
-    <div className="col-md-3">
-      <Departments authorities={data.authorities} />
-    </div>
-    <div className="col-md-2">
-      <IpList label={I18n.t('OPERATOR_PROFILE.IP_LIST.TITLE')} ips={ips} />
+  <div className="player__account__details">
+    <div className="row">
+      <div className="col-md-4">
+        <Personal data={data} />
+      </div>
+      <div className="col-md-3">
+        <Departments authorities={data.authorities} />
+      </div>
+      <div className="col-md-2">
+        <IpList label={I18n.t('OPERATOR_PROFILE.IP_LIST.TITLE')} ips={ips} />
+      </div>
     </div>
   </div>
 );
--- src/routes/UserProfile/components/Information/Information.js
+++ src/routes/UserProfile/components/Information/Information.js
@@ -26,28 +26,30 @@ class Information extends Component {
     } = this.props;
 
     return (
-      <div className="player__account__details row">
-        <div className="col-md-4">
-          <Personal data={data} />
-        </div>
-        <div className="col-md-2">
-          <Additional
-            initialValues={{
-              marketingMail: data.marketingMail,
-              marketingNews: data.marketingNews,
-              marketingSMS: data.marketingSMS,
-            }}
-            updateSubscription={updateSubscription}
-          />
-        </div>
-        <div className="col-md-2">
-          <IpList label={I18n.t('PLAYER_PROFILE.IP_LIST.TITLE')} ips={ips} />
-        </div>
-        <div className="col-md-4">
-          <Notes
-            notes={notes}
-            onEditNoteClick={onEditNoteClick}
-          />
+      <div className="player__account__details">
+        <div className="row">
+          <div className="col-md-4">
+            <Personal data={data} />
+          </div>
+          <div className="col-md-2">
+            <Additional
+              initialValues={{
+                marketingMail: data.marketingMail,
+                marketingNews: data.marketingNews,
+                marketingSMS: data.marketingSMS,
+              }}
+              updateSubscription={updateSubscription}
+            />
+          </div>
+          <div className="col-md-2">
+            <IpList label={I18n.t('PLAYER_PROFILE.IP_LIST.TITLE')} ips={ips} />
+          </div>
+          <div className="col-md-4">
+            <Notes
+              notes={notes}
+              onEditNoteClick={onEditNoteClick}
+            />
+          </div>
         </div>
       </div>
     );
