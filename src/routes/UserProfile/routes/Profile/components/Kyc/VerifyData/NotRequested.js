a8e563b4e7e56e0a326bf82d04a4c0de6e2d7d83
KostyaDyachenko <kd@newagesol.com>
2017-08-03 11:55:36 +0300

    KYC markup ready.

--- src/routes/UserProfile/routes/Profile/components/Kyc/VerifyData/NotRequested.js
+++ src/routes/UserProfile/routes/Profile/components/Kyc/VerifyData/NotRequested.js
@@ -3,7 +3,7 @@ import { I18n } from 'react-redux-i18n';
 import PropTypes from '../../../../../../../constants/propTypes';
 
 const NotRequested = props => (
-  <div className=" margin-bottom-10">
+  <div className="margin-bottom-10">
     <div className="font-size-18 font-weight-700 color-secondary">
       <i className="fa fa-check-circle-o" /> {' '}
       {I18n.t('PLAYER_PROFILE.PROFILE.KYC_VERIFICATION.TYPE_NOT_REQUESTED', { title: props.title })}
