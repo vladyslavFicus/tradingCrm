defdb8161178ce183a63c87328d9432bfcbea0e4
intpp <intpplus@gmail.com>
2017-08-15 16:46:49 +0300

    change nginx template path

--- build/scripts/docker/run.js
+++ build/scripts/docker/run.js
@@ -44,7 +44,7 @@ function processError(error) {
 }
 
 function compileNginxConfig(environmentConfig) {
-  let config = fs.readFileSync('./nginx.conf.tpl');
+  let config = fs.readFileSync('/opt/docker/nginx.conf.tpl');
   const params = {
     logstashUrl: environmentConfig.logstash
       ? environmentConfig.logstash.url
