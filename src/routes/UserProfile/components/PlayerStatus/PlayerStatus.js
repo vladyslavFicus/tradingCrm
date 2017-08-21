d46c2d850d4095e5968a7eff4b221956bf30afd4
vitaliy <khotey.v@ya.ru>
2017-08-21 17:30:14 +0300

    fixed fullname check

--- src/routes/UserProfile/components/PlayerStatus/PlayerStatus.js
+++ src/routes/UserProfile/components/PlayerStatus/PlayerStatus.js
@@ -127,7 +127,7 @@ class PlayerStatus extends Component {
           {statusesLabels[status]}
         </div>
         {
-          !!reason &&
+          !!fullName &&
           <div className="header-block-small">
             by {fullName}
           </div>
