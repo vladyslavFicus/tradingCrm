151340ed16fedf9cb06bb8266239ba4053c32dfc
KostyaDyachenko <kd@newagesol.com>
2017-08-23 13:41:40 +0300

    Mini profile for Player ready

--- src/components/MiniProfile/MiniProfile.js
+++ src/components/MiniProfile/MiniProfile.js
@@ -1,89 +1,55 @@
-import React, { Component } from 'react';
-import { Popover, PopoverContent } from 'reactstrap';
-//import classNames from 'classnames';
-import PropTypes from '../../constants/propTypes';
-// import { entitiesPrefixes } from '../../constants/uuid';
+import React from 'react';
 import './MiniProfile.scss';
 
-class MiniProfile extends Component {
-  static propTypes = {
-    isOpen: PropTypes.bool,
-    placement: PropTypes.string,
-    labelStatus: PropTypes.string,
-    target: PropTypes.string.isRequired,
-    profileType: PropTypes.string.isRequired,
-  };
-  static defaultProps = {
-    isOpen: false,
-    placement: 'right',
-    labelStatus: 'inactive',
-    profileType: 'Player', /* delete */
-  };
-
-  renderHeader = () => {
-    return (
-      <div className="mini-profile_header">
-        <label className={`mini-profile-label label-inactive`}>inactive</label>
-        <div className="mini-profile-type">Player</div>
-        <div className="mini-profile-title">
-          <b>Jimmy Black</b> (29)
-          <img src="assets/img/kyc-check-icon.svg" alt="check" />
-          <img src="assets/img/pinned-note-icon.png" alt="note" />
-        </div>
-        <div className="mini-profile-subtitle">
-          <span>jimmyjimmy</span> - PL-9f2274d3
-        </div>
-        <div className="mini-profile_tags" />
+const MiniProfile = () => (
+  <div className="mini-profile mini-profile_inactive">
+    <div className="mini-profile-header">
+      <label className="mini-profile-label">inactive</label>
+      <div className="mini-profile-type">Player</div>
+      <div className="mini-profile-title">
+        <b>Jimmy Black</b> (29)
+        <i className="note-icon note-pinned-note" />
       </div>
-    );
-  };
-
-  renderBody = () => {
-    return (
-      <div className="mini-profile_body">
-        <div className="info-block">
-          <div className="info-block_left">balance</div>
-          <div className="info-block_right">€ 20 205,00</div>
-        </div>
-        <div className="info-block">
-          <div className="info-block_left">last login</div>
-          <div className="info-block_right">1 Month, 10 Days ago</div>
-        </div>
-        <div className="info-block">
-          <div className="info-block_left">last login</div>
-          <div className="info-block_right">1 Month, 10 Days ago</div>
-        </div>
-        <div className="info-block">
-          <div className="info-block_left">last login</div>
-          <div className="info-block_right">1 Month, 10 Days ago</div>
-        </div>
+      <div className="mini-profile-ids">
+        <span>jimmyjimmy</span> - PL-9f2274d3
       </div>
-    );
-  };
-
-  renderStatusReasonBody = () => {
-    return (
-      <div className="mini-profile_body">
-        <div className="info-block">
-          <div className="info-block_status-reason">status reason</div>
-          <div className="info-block_status-reason_body">The reason that was selected on account status change</div>
-        </div>
+      <div className="mini-profile-tags">
+        <span className="mini-profile-tag mini-profile-tag_danger">
+          Текст тега
+        </span>
+        <span className="mini-profile-tag mini-profile-tag_danger">
+          One more bad tag
+        </span>
+        <span className="mini-profile-tag mini-profile-tag_success">
+          Текст зеленого тега
+        </span>
       </div>
-    );
-  };
+    </div>
+    <div className="mini-profile-status-reason">
+      <div className="info-block">
+        <div className="info-block_status-reason">status reason</div>
+        <div className="info-block_status-reason_body">The reason that was selected on account status change</div>
+      </div>
+    </div>
+    <div className="mini-profile-content">
+      <div className="info-block">
+        <div className="info-block-label">balance</div>
+        <div className="info-block-content">€ 20 205,00</div>
+      </div>
+      <div className="info-block">
+        <div className="info-block-label">last login</div>
+        <div className="info-block-content">1 Month, 10 Days ago</div>
+      </div>
+      <div className="info-block">
+        <div className="info-block-label">last login</div>
+        <div className="info-block-content">1 Month, 10 Days ago</div>
+      </div>
+      <div className="info-block">
+        <div className="info-block-label">last login</div>
+        <div className="info-block-content">1 Month, 10 Days ago</div>
+      </div>
+    </div>
+  </div>
+);
 
-  render() {
-    const { isOpen, placement, target } = this.props;
-
-    return (
-      <Popover isOpen={isOpen} placement={placement} target={target} className="mini-profile">
-        <PopoverContent>
-          {this.renderHeader()}
-          {this.renderStatusReasonBody()}
-          {this.renderBody()}
-        </PopoverContent>
-      </Popover>
-    );
-  }
-}
 export default MiniProfile;
