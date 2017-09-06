46a055ccb0aae6969783d11c60dcb4db88de1ac8
KostyaDyachenko <kd@newagesol.com>
2017-09-06 10:25:29 +0300

    commit

--- src/routes/UserProfile/routes/GameActivity/components/GameRoundType/GameRoundType.js
+++ src/routes/UserProfile/routes/GameActivity/components/GameRoundType/GameRoundType.js
@@ -1,52 +1,15 @@
-import React, { Component } from 'react';
-import PropTypes from '../../../../../../constants/propTypes';
-import { types, typesLabels, typesProps } from './constants';
-import renderLabel from '../../../../../../utils/renderLabel';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './GameRoundType.scss';
 
-class GameRoundType extends Component {
-  static propTypes = {
-    gameRound: PropTypes.shape({
-      gameRoundType: PropTypes.string,
-    }).isRequired,
-  };
+const GameRoundType = ({ gameRound }) => (
+  <div className="game-round-type">test</div>
+);
 
-  render() {
-    const { gameRound } = this.props;
-
-    return (
-      <div>tes</div>
-    );
-
-
-    /*if (gameRound.gameRoundType === 'FREE_SPIN') {
-      return (
-        <div className="font-size-11 color-success font-weight-700">
-          FREE SPIN
-        </div>
-      );
-    } else if (gameRound.gameRoundType === 'BONUS_ROUND') {
-      return (
-        <div className="font-size-11 color-primary font-weight-700">
-          BONUS ROUND
-        </div>
-      );
-    } else if (gameRound.gameRoundType === 'SPORTBET') {
-      return (
-        <div className="font-size-11 color-primary font-weight-700">
-          SPORTSBET
-        </div>
-      );
-    } else if (gameRound.gameRoundType === 'OTHER') {
-      return (
-        <div className="font-size-11 color-primary font-weight-700">
-          OTHER
-        </div>
-      );
-    }
-
-    return null;*/
-  }
-}
+GameRoundType.propTypes = {
+  gameRound: PropTypes.shape({
+    gameRoundType: PropTypes.string,
+  }).isRequired,
+};
 
 export default GameRoundType;
