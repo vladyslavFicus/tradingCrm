766eff2c66dfe90305eb575b714117d05ec9f0a5
KostyaDyachenko <kd@newagesol.com>
2017-10-02 16:21:10 +0300

    Status dropdowns arrow onMouseOver

--- src/components/FileStatusDropDown/FileStatusDropDown.js
+++ src/components/FileStatusDropDown/FileStatusDropDown.js
@@ -5,7 +5,6 @@ import classNames from 'classnames';
 import { I18n } from 'react-redux-i18n';
 import Uuid from '../../components/Uuid';
 import { statuses, statusActions, statusesColorNames, statusesLabels, actionsColorNames } from '../../constants/files';
-import './FileStatusDropDown.scss';
 
 class FileStatusDropDown extends Component {
   static propTypes = {
@@ -29,7 +28,7 @@ class FileStatusDropDown extends Component {
 
     const label = (
       <div>
-        <div className={classNames('font-weight-700 file-status', statusesColorNames[status.value])}>
+        <div className={classNames('font-weight-700 status', statusesColorNames[status.value])}>
           {
             statusesLabels[status.value]
               ? statusesLabels[status.value]
@@ -51,28 +50,26 @@ class FileStatusDropDown extends Component {
     }
 
     return (
-      <div className="file-status-dropdown">
-        <Dropdown isOpen={dropDownOpen} toggle={this.toggle}>
-          <span onClick={this.toggle} className="cursor-pointer">
-            {label}
-          </span>
-          <DropdownMenu>
-            {
-              statusActions[status.value].map(item => (
-                <DropdownItem
-                  onClick={() => this.props.onStatusChange(item.action)}
-                  className={classNames('text-uppercase', actionsColorNames[item.action])}
-                  key={item.label}
-                >
-                  <div className="font-weight-700">
-                    {item.label}
-                  </div>
-                </DropdownItem>
-              ))
-            }
-          </DropdownMenu>
-        </Dropdown>
-      </div>
+      <Dropdown isOpen={dropDownOpen} toggle={this.toggle} className="status-dropdown">
+        <span onClick={this.toggle} className="cursor-pointer">
+          {label}
+        </span>
+        <DropdownMenu>
+          {
+            statusActions[status.value].map(item => (
+              <DropdownItem
+                onClick={() => this.props.onStatusChange(item.action)}
+                className={classNames('text-uppercase', actionsColorNames[item.action])}
+                key={item.label}
+              >
+                <div className="font-weight-700">
+                  {item.label}
+                </div>
+              </DropdownItem>
+            ))
+          }
+        </DropdownMenu>
+      </Dropdown>
     );
   }
 }
