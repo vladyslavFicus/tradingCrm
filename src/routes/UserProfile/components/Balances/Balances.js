f01c7443c78aeea76985ed7c2dac43ea0fa72917
KostyaDyachenko <kd@newagesol.com>
2017-10-11 17:33:32 +0300

    Balance Withdrawable amount markup

--- src/routes/UserProfile/components/Balances/Balances.js
+++ src/routes/UserProfile/components/Balances/Balances.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
 import classNames from 'classnames';
+import { I18n } from 'react-redux-i18n';
 import Amount from '../../../../components/Amount';
 import PropTypes from '../../../../constants/propTypes';
 
@@ -13,7 +14,7 @@ class Balances extends Component {
       total: PropTypes.price,
       bonus: PropTypes.price,
       real: PropTypes.price,
-    }),
+    }).isRequired,
   };
 
   state = {
@@ -29,16 +30,19 @@ class Balances extends Component {
   renderDropDown = (label, balances, dropDownOpen) => (
     <Dropdown isOpen={dropDownOpen} toggle={this.toggle} onClick={this.toggle}>
       {label}
-
       <DropdownMenu>
         <div className="dropdown-menu__content">
           <DropdownItem>
+            <div className="amount">€0.000</div>
+            <div className="amount_label">{I18n.t('PLAYER_PROFILE.PROFILE.BALANCES_DROPDOWN.WITHDRAWABLE')}</div>
+          </DropdownItem>
+          <DropdownItem>
             <div className="amount"><Amount {...balances.deposits} /></div>
-            <div className="amount_label">Deposited</div>
+            <div className="amount_label">{I18n.t('PLAYER_PROFILE.PROFILE.BALANCES_DROPDOWN.DEPOSITED')}</div>
           </DropdownItem>
           <DropdownItem>
             <div className="amount"><Amount {...balances.withdraws} /></div>
-            <div className="amount_label">Withdrawn</div>
+            <div className="amount_label">{I18n.t('PLAYER_PROFILE.PROFILE.BALANCES_DROPDOWN.WITHDRAWN')}</div>
           </DropdownItem>
         </div>
       </DropdownMenu>
