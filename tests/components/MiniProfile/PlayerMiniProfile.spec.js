0abb53b7218272ff859f82f27b995aec3b4422b6
VS <slisarenko.vlad@gmail.com>
2017-11-06 10:13:34 +0200

    simple test

---
+++ tests/components/MiniProfile/PlayerMiniProfile.spec.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import PlayerMiniProfile from '../../../src/components/MiniProfile/PlayerMiniProfile';
+import Uuid from '../../../src/components/Uuid';
+import { statuses as userStatuses } from '../../../src/constants/user';
+
+describe('(Component) PlayerMiniProfile', () => {
+  let _props, _wrapper;
+
+  const playerUUID = "PLAYER-3fd80a6e-931d-43b4-9f71-e639b9ea8045";
+
+  const playerUUIDContainer = <Uuid
+    uuid={playerUUID}
+    uuidPrefix={playerUUID.indexOf('PLAYER') === -1 ? 'PL' : null}
+  />;
+
+  beforeEach(() => {
+    _props = {
+      data: {
+        signInIps: [],
+        profileStatus: userStatuses.ACTIVE,
+        username: "",
+        playerUUID,
+        profileStatusReason: "",
+        balances: {
+          bonus: { amount: 0, currency: "EUR" },
+          real: { amount: 0, currency: "EUR" },
+          total: { amount: 0, currency: "EUR" },
+          withdrawable: { amount: 0, currency: "EUR" },
+        },
+        tags: [
+          {id: 14430, priority: 'neutral', tag: 'tag2'},
+          {id: 14431, priority: 'negative', tag: 'tag2'},
+        ],
+      }
+    };
+    _wrapper = shallow(<PlayerMiniProfile {..._props} />);
+  });
+
+  it('renders as a <div>.', () => {
+    expect(_wrapper.is('div')).to.equal(true);
+  });
+
+  it('render check icon', () => {
+    _props = {
+      data: {
+        ..._props.data,
+        kycCompleted: true,
+      }
+    }
+    _wrapper = shallow(<PlayerMiniProfile {..._props} />);
+    expect(_wrapper.find('i.fa.fa-check')).to.exist();
+  });
+
+  it('do not render check icon', () => {
+    _wrapper = shallow(<PlayerMiniProfile {..._props} />);
+    expect(_wrapper.find('i.fa.fa-check')).to.not.exist();
+  });
+
+  it('render tags', () => {
+    _props.data.tags.map(tag => {
+      expect(_wrapper.find(`span.mini-profile-tag_${tag.priority}`)).to.exist();
+    })
+  });
+
+  it('render reason', () => {
+    _props = {
+      data: {
+        ..._props.data,
+        profileStatus: userStatuses.BLOCKED
+      }
+    };
+    _wrapper = shallow(<PlayerMiniProfile {..._props} />);
+    expect(_wrapper.find('div.info-block_status-reason_body')).to.exist();
+
+    _props = {
+      data: {
+        ..._props.data,
+        profileStatus: userStatuses.SUSPENDED
+      }
+    };
+    _wrapper = shallow(<PlayerMiniProfile {..._props} />);
+    expect(_wrapper.find('div.info-block_status-reason_body')).to.exist();
+  });
+
+  it('render UUID', () => {
+    expect(_wrapper).to.contain(playerUUIDContainer);
+  });
+
+  it('do not render UUID', () => {
+    _props = {
+      data: {
+        ..._props.data,
+        playerUUID: null,
+      }
+    };
+    _wrapper = shallow(<PlayerMiniProfile {..._props} />);
+
+    expect(_wrapper).to.not.contain(playerUUIDContainer);
+  });
+});
