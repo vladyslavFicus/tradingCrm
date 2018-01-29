ded7d0b5c4db4da7257f24d8d5ba8887de783d65
okhivrich <alexandr.khivrich@gmail.com>
2018-01-29 19:21:25 +0200

    added create route

--- src/routes/BonusCampaigns/index.js
+++ src/routes/BonusCampaigns/index.js
@@ -1,10 +1,12 @@
 import ListRoute from './routes/List';
 import ViewRoute from './routes/View';
+import CreateRoute from './routes/Create';
 
 export default store => ({
   path: '/bonus-campaigns',
   indexRoute: ListRoute(store),
   childRoutes: [
     ViewRoute(store),
+    CreateRoute(store),
   ],
 });
