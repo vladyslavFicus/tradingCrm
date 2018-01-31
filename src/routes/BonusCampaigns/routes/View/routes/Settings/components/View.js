8540cb1f7f23e2c63f40679a2b1be6a69986c4cc
VS <slisarenko.vlad@gmail.com>
2018-01-31 12:21:18 +0200

    polling FST

--- src/routes/BonusCampaigns/routes/View/routes/Settings/components/View.js
+++ src/routes/BonusCampaigns/routes/View/routes/Settings/components/View.js
@@ -13,6 +13,7 @@ import AddToCampaignModal from '../../../../../../../components/AddToCampaignMod
 
 const CURRENCY_AMOUNT_MODAL = 'currency-amount-modal';
 const CHOOSE_CAMPAIGN_MODAL = 'choose-campaign-modal';
+const POLLING_FREE_SPIN_TEMPLATE_INTERVAL =  1000;
 const modalInitialState = {
   name: null,
   params: {},
@@ -115,7 +116,7 @@ class View extends Component {
             resolve();
           }
         }
-      }, 1000);
+      }, POLLING_FREE_SPIN_TEMPLATE_INTERVAL);
     });
 
     return promise;
