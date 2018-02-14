e24e66a0c470f43e1765fc251d03d946a0c58d0b
VS <slisarenko.vlad@gmail.com>
2018-02-14 19:26:22 +0200

    remove UNDEFINED action.payload.response from IF

--- src/redux/middlewares/apiVersion.js
+++ src/redux/middlewares/apiVersion.js
@@ -6,8 +6,7 @@ const regExp = new RegExp('-failure$');
 export default store => next => (action) => {
   if (
     action && action.error && regExp.test(action.type)
-    && action.payload.response && action.payload.status === 426
-    && (!action.payload.response.message || action.payload.response.message !== 'JWT_TOKEN_EXPIRED')
+    && action.payload && action.payload.status === 426
   ) {
     store.dispatch(modalActionCreators.open({ name: modalTypes.NEW_API_VERSION }));
   }
