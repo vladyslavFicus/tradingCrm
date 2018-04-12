40392944aee3f621809ffa2ff63eff77065be002
okhivrich <a.khivrych@newagesol.com>
2018-04-12 16:46:13 +0300

    update campaign

--- src/graphql/queries/campaigns.js
+++ src/graphql/queries/campaigns.js
@@ -100,56 +100,6 @@ const freeSpinTemplateQuery = gql`query freeSpinTemplate($uuid: String!, $aggreg
   }
 }`;
 
-const freeSpinTemplateMutation = gql`mutation freeSpinTemplateMutation(
-  $name: String!,
-  $aggregatorId: String!
-  $providerId: String!
-  $gameId: String!
-  $comment: String
-  $betMultiplier: Int
-  $coinSize: Int
-  $freeSpinLifeTime: Int!
-  $freeSpinsAmount: Int!
-  $linesPerSpin: Int
-  $rhfpBet: Int
-  $pageCode: String
-  $betLevel: Int
-  $betPerLineAmounts: [InputMoney]
-  $bonusTemplateUUID: String
-  ) {
-  freeSpinTemplate {
-    add (
-    name: $name,
-    aggregatorId: $aggregatorId,
-    providerId: $providerId,
-    gameId: $gameId,
-    comment: $comment,
-    betMultiplier: $betMultiplier,
-    coinSize: $coinSize,
-    freeSpinLifeTime: $freeSpinLifeTime,
-    freeSpinsAmount: $freeSpinsAmount,
-    linesPerSpin: $linesPerSpin,
-    rhfpBet: $rhfpBet,
-    betLevel: $betLevel,
-    betPerLineAmounts: $betPerLineAmounts
-    bonusTemplateUUID: $bonusTemplateUUID
-    pageCode: $pageCode
-    ) {
-      data {
-        aggregatorId
-        providerId
-        name
-        status
-        uuid
-      }
-      error {
-        error
-        fields_errors
-      }
-    }
-  }
-}`;
-
 const freeSpinTemplateOptionsQuery = gql`query freeSpinOptions {
   freeSpinOptions
 }`;
@@ -177,6 +127,5 @@ export {
   shortBonusTemplatesQuery,
   bonusTemplateQuery,
   freeSpinTemplateOptionsQuery,
-  freeSpinTemplateMutation,
   wageringQuery,
 };
