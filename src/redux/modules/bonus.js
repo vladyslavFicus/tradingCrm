95c2a7f7f46900557a59e215c3f445793273c1c3
VS <slisarenko.vlad@gmail.com>
2018-04-20 11:19:32 +0300

    show error message when try to confiscate payment, when player has active bonus

---
+++ src/redux/modules/bonus.js
@@ -0,0 +1,30 @@
+import { CALL_API } from 'redux-api-middleware';
+import buildQueryString from '../../utils/buildQueryString';
+
+function fetchActiveBonus(type) {
+  return playerUUID => (dispatch, getState) => {
+    const { auth: { token, logged } } = getState();
+
+    return dispatch({
+      [CALL_API]: {
+        endpoint: `bonus/bonuses/${playerUUID}?${buildQueryString({ states: 'IN_PROGRESS' })}`,
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        types: [type.REQUEST, type.SUCCESS, type.FAILURE],
+        bailout: !logged,
+      },
+    });
+  };
+}
+
+const sourceActionCreators = {
+  fetchActiveBonus,
+};
+
+export {
+  sourceActionCreators,
+};
