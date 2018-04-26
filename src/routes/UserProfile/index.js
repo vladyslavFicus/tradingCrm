623992ebb02019897e10695d8a83f29160fce244
VS <slisarenko.vlad@gmail.com>
2018-04-26 16:24:06 +0300

    use latest version

--- src/routes/UserProfile/index.js
+++ src/routes/UserProfile/index.js
@@ -33,6 +33,7 @@ export default store => ({
             login: '',
             uuid: playerUUID,
             path: location.pathname.replace(`/${PLAYER_PROFILE_ROUTE_PREFIX}/${playerUUID}/`, ''),
+            brandId,
             authorId: uuid,
           }));
           replace({ pathname: `/${PLAYER_PROFILE_ROUTE_PREFIX}/list`, state: { ignoreByUsersPanel: true } });
