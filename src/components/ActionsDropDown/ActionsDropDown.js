4f8ca6e0cfc6ad08b9b9eb04fc9c9223e8759919
kostyamba <kd@newagesol.com>
2018-05-03 15:50:44 +0300

    DropDown component

--- src/components/ActionsDropDown/ActionsDropDown.js
+++ src/components/ActionsDropDown/ActionsDropDown.js
@@ -1,54 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
+import { DropdownItem } from 'reactstrap';
+import DropDown from '../DropDown';
 
-const ActionsDropDowns = PropTypes.shape({
-  label: PropTypes.string,
-  onClick: PropTypes.func,
-});
+const ActionsDropDown = ({ items }) => {
+  const visibleItems = items.filter(item => item.visible === undefined || item.visible);
 
-class ActionsDropDown extends Component {
-  static propTypes = {
-    label: PropTypes.any,
-    items: PropTypes.arrayOf(ActionsDropDowns),
-  };
+  return (
+    <DropDown
+      className="d-inline-block"
+      toggleClassName="btn btn-sm btn-default-outline"
+      withArrow
+      rightPlacement
+    >
+      {visibleItems.map(item => (
+        <DropdownItem
+          id={item.id}
+          key={item.label}
+          onClick={item.onClick}
+        >
+          {item.label}
+        </DropdownItem>
+      ))}
+    </DropDown>
+  );
+};
 
-  static defaultProps = {
-    label: <i className="fa fa-caret-down" />,
-    items: [],
-  };
-
-  state = {
-    isOpened: false,
-  };
-
-  handleToggleDropDownState = () => {
-    this.setState({ isOpened: !this.state.isOpened });
-  };
-
-  render() {
-    const { isOpened } = this.state;
-    const { label, items } = this.props;
-    const visibleItems = items.filter(item => item.visible === undefined || item.visible);
-
-    return (
-      <Dropdown isOpen={isOpened} toggle={this.handleToggleDropDownState}>
-        <button className="btn btn-sm btn-default-outline" onClick={this.handleToggleDropDownState}>
-          {label}
-        </button>
-
-        <DropdownMenu right>
-          {visibleItems.map(item => (
-            <DropdownItem
-              id={item.id}
-              key={item.label}
-              onClick={item.onClick}
-            >{item.label}</DropdownItem>
-          ))}
-        </DropdownMenu>
-      </Dropdown>
-    );
-  }
-}
+ActionsDropDown.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    onClick: PropTypes.func,
+  })).isRequired,
+};
 
 export default ActionsDropDown;
