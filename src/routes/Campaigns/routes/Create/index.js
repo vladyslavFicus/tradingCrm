25e60c677482f6ef7f90c3d0410b09c56555e854
okhivrich <alexandr.khivrich@gmail.com>
2018-05-17 13:58:29 +0300

    new campaigns create

--- src/routes/Campaigns/routes/Create/index.js
+++ src/routes/Campaigns/routes/Create/index.js
@@ -1,17 +1,5 @@
-import Permissions from '../../../../utils/permissions';
-import permission from '../../../../config/permissions';
+import { asyncRoute } from '../../../../router';
 
-const requiredPermissions = new Permissions([permission.CAMPAIGNS.CREATE]);
+export default asyncRoute(() =>
+  import(/* webpackChunkName: "CampaignCreateContainer" */ './containers/CampaignCreateContainer'));
 
-export default store => ({
-  path: 'create',
-  getComponent(nextState, cb) {
-    require.ensure([], (require) => {
-      if (!requiredPermissions.check(store.getState().permissions.data)) {
-        return cb(null, require('../../../Forbidden/containers/ForbiddenContainer').default);
-      }
-
-      cb(null, require('./container/CampaignCreateContainer').default);
-    }, 'campaign-create');
-  },
-});
