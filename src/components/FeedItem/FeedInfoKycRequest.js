7b88d7b6ac2aff21edad0e3b556204a9e8dbaf41
kostyamba <kd@newagesol.com>
2018-08-08 14:44:15 +0300

    FeedItem markup changed

--- src/components/FeedItem/FeedInfoKycRequest.js
+++ src/components/FeedItem/FeedInfoKycRequest.js
@@ -7,12 +7,11 @@ import renderLabel from '../../utils/renderLabel';
 const FeedInfoKycRequest = ({ data }) => (
   <Fragment>
     {I18n.t('FEED_ITEM.KYC.LOGIN_EVENTS.REASON')}:
-    <span className="feed-item__content-value">
-      {
-        data.details.reason &&
-        renderLabel(data.details.reason, verifyRequestReasons)
-      }
-    </span>
+    <If condition={data.details.reason}>
+      <span className="feed-item__content-value">
+        {renderLabel(data.details.reason, verifyRequestReasons)}
+      </span>
+    </If>
   </Fragment>
 );
 
