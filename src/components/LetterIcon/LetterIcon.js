c1894f677c2d5893580c506e08d7b9ea66f4d6e6
VS <slisarenko.vlad@gmail.com>
2018-09-13 12:17:34 +0300

    replace letter-icon css to component folder

--- src/components/LetterIcon/LetterIcon.js
+++ src/components/LetterIcon/LetterIcon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import './LetterIcon.scss';
 
 const LetterIcon = ({ color, letter }) => (
   <div className={classNames('letters-icon', color)}>
