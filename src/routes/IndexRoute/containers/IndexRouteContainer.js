920b92cdd62229e12551b41bdeadeb3d8f9ff8b1
Eugene Husak <ehusak@newagesol.com>
2019-01-23 18:50:58 +0200

    [FAL-127] - added permissions to client profile sales/retention update

--- src/routes/IndexRoute/containers/IndexRouteContainer.js
+++ src/routes/IndexRoute/containers/IndexRouteContainer.js
@@ -1,13 +1,11 @@
 import { connect } from 'react-redux';
+import { compose } from 'react-apollo';
 import { withRouter } from 'react-router-dom';
 import IndexRoute from '../components/IndexRoute';
 
-export default withRouter(
-  connect(({
-    auth: {
-      logged,
-    },
-  }) => ({
-    logged,
-  }))(IndexRoute)
-);
+const mapStateToProps = ({ auth: { logged } }) => ({ logged });
+
+export default compose(
+  withRouter,
+  connect(mapStateToProps)
+)(IndexRoute);
