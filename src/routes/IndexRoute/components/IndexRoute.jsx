3548f6d67d0d7efb8609bde4b1932ab04dd1fb00
Eugene Husak <ehusak@newagesol.com>
2019-02-28 11:03:20 +0200

    fix: one more fix masters branch

--- src/routes/IndexRoute/components/IndexRoute.jsx
+++ src/routes/IndexRoute/components/IndexRoute.jsx
@@ -65,9 +65,9 @@ class IndexRoute extends PureComponent {
           <AppRoute path="/hierarchy" layout={MainLayout} component={Hierarchy} checkAuth />
           <AppRoute path="/operators" layout={MainLayout} component={Operators} checkAuth />
           <AppRoute path="/partners" layout={MainLayout} component={Partners} checkAuth />
-          <AppRoute path="/offices" layout={MainLayout} component={Offices} checkAuth checkAdmin />
-          <AppRoute path="/desks" layout={MainLayout} component={Desks} checkAuth checkAdmin />
-          <AppRoute path="/teams" layout={MainLayout} component={Teams} checkAuth checkAdmin />
+          <AppRoute path="/offices" layout={MainLayout} component={Offices} checkAuth />
+          <AppRoute path="/desks" layout={MainLayout} component={Desks} checkAuth />
+          <AppRoute path="/teams" layout={MainLayout} component={Teams} checkAuth />
           <AppRoute path="/release-notes" layout={MainLayout} component={ReleaseNotes} checkAuth />
           <Route path="/logout" component={Logout} />
           {/* Public */}
