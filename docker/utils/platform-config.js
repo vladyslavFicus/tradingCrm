6cb3b6ff18aaa7f08b8f0f9bc4c59c28c636298c
Igor <igorztsorokopud@gmail.com>
2019-11-28 15:34:07 +0200

    Added handler for application-config

--- docker/utils/platform-config.js
+++ docker/utils/platform-config.js
@@ -10,4 +10,12 @@ if (!NAS_PROJECT) {
   throw new Error('Missing required environment variable "NAS_PROJECT"');
 }
 
-module.exports = ymlReader.load(path.resolve(SECRET_PATH, `application-${NAS_PROJECT}.yml`));
+let config;
+
+try {
+  config = ymlReader.load(path.resolve(SECRET_PATH, 'application-config.yml'));
+} catch (e) {
+  config = ymlReader.load(path.resolve(SECRET_PATH, `application-${NAS_PROJECT}.yml`));
+}
+
+module.exports = config;
