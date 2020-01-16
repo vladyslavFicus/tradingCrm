3f1757d7ae8a5cbdbcb0ece2d68d5b32c536a02c
Sergey Tkachenko <stkachenko@overonix.com>
2020-01-16 20:05:16 +0000

    Merged in i18n-missing-translations (pull request #184)
    I18n: add I18n handler to missing translation issue
    
    * I18n: add I18n handler to missing translation issue
    
    Approved-by: Yevhen Savitskyi
    Approved-by: Artem Polischuk

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,17 +1,18 @@
 import I18n from 'i18n-js';
-
-// Import moment locales
 import 'moment/locale/zh-cn';
-
-// Import application locales
 import en from './en.json';
 // import zh from './zh.json';
 
-I18n.translations = {
-  en,
-  // zh,
-};
-
+I18n.translations = { en };
 I18n.defaultLocale = 'en';
+
+/**
+ *
+ * If there is no translation in the file with translations for the value.
+ * Instead of the error [missing "EN.Your value" translation]
+ * the transferred value will be displayed - "Your value"
+ *
+*/
+I18n.missingTranslation = value => value;
 
 export default I18n;
