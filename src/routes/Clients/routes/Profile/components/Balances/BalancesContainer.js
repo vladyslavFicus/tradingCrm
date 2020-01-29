8a5a18a83f262e8ab3081aa8e04dbf159dd94cba
Yevhen Savitskyi <ysavitskyi@overonix.com>
2020-01-29 12:51:19 +0000

    FS-32 total balance dropdown

--- src/routes/Clients/routes/Profile/components/Balances/BalancesContainer.js
+++ src/routes/Clients/routes/Profile/components/Balances/BalancesContainer.js
@@ -1,28 +1,10 @@
 import { graphql, compose } from 'react-apollo';
-import { connect } from 'react-redux';
-import moment from 'moment';
-import { reduxForm, formValueSelector } from 'redux-form';
 import { paymentsStatisticQuery } from 'graphql/queries/statistics';
 import { tradingTypes, tradingStatuses } from 'constants/payment';
-import { formName, initialQueryParams } from './constants';
+import { initialQueryParams } from './constants';
 import Balances from './Balances';
 
-const selector = formValueSelector(formName);
-
-const reduxFormConfig = reduxForm({
-  form: formName,
-  initialValues: {
-    date: moment().subtract(7, 'days').format(),
-  },
-});
-
-const mapStateToProps = state => ({
-  selectValue: selector(state, 'date'),
-});
-
 export default compose(
-  connect(mapStateToProps),
-  reduxFormConfig,
   graphql(paymentsStatisticQuery, {
     options: ({ uuid }) => ({
       variables: {
--- src/routes/Clients/routes/Profile/components/Balances/constants.js
+++ src/routes/Clients/routes/Profile/components/Balances/constants.js
@@ -1,34 +1,37 @@
 import moment from 'moment';
 
-export const formName = 'balanceForm';
-
-export const selectItems = [{
-  label: 'CLIENT_PROFILE.CLIENT.BALANCES.PERIOD_PRESETS.LAST_7_DAYS',
-  value: moment()
-    .subtract(6, 'days')
-    .startOf('day')
-    .format(),
-}, {
-  label: 'CLIENT_PROFILE.CLIENT.BALANCES.PERIOD_PRESETS.LAST_MONTH',
-  value: moment()
-    .subtract(1, 'month')
-    .add(1, 'days')
-    .startOf('day')
-    .format(),
-}, {
-  label: 'CLIENT_PROFILE.CLIENT.BALANCES.PERIOD_PRESETS.LAST_3_MONTHS',
-  value: moment()
-    .subtract(3, 'month')
-    .add(1, 'days')
-    .startOf('day')
-    .format(),
-}];
+export const selectItems = [
+  {
+    label: 'CLIENT_PROFILE.CLIENT.BALANCES.PERIOD_PRESETS.ALL_TIME',
+    value: '',
+  },
+  {
+    label: 'CLIENT_PROFILE.CLIENT.BALANCES.PERIOD_PRESETS.LAST_7_DAYS',
+    value: moment()
+      .subtract(6, 'days')
+      .startOf('day')
+      .format(),
+  },
+  {
+    label: 'CLIENT_PROFILE.CLIENT.BALANCES.PERIOD_PRESETS.LAST_MONTH',
+    value: moment()
+      .subtract(1, 'month')
+      .add(1, 'days')
+      .startOf('day')
+      .format(),
+  },
+  {
+    label: 'CLIENT_PROFILE.CLIENT.BALANCES.PERIOD_PRESETS.LAST_3_MONTHS',
+    value: moment()
+      .subtract(3, 'month')
+      .add(1, 'days')
+      .startOf('day')
+      .format(),
+  },
+];
 
 export const initialQueryParams = (paymentType, paymentStatus) => ({
-  dateFrom: moment()
-    .subtract(6, 'days')
-    .startOf('day')
-    .format(),
+  dateFrom: '',
   dateTo: moment()
     .add(1, 'day')
     .startOf('day')
