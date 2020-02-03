d98fdd8ee7a06ee9015a2be11a02406d64dfb219
Sergey Tkachenko <stkachenko@overonix.com>
2020-02-03 09:29:58 +0200

    Refactoring: moved AppContainer from src/containers into root src folder like on client portal, moved ApolloProvider from graphql to apollo folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Suspense, PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import TokenRenew from 'components/TokenRenew';
-import LocalStorageListener from 'providers/LocalStorageListener';
+import PropTypes from 'prop-types';
+import ApolloProvider from 'apollo/ApolloProvider';
 import StorageProvider from 'providers/StorageProvider';
-import ApolloProvider from 'graphql/ApolloProvider';
+import LocalStorageListener from 'providers/LocalStorageListener';
+import TokenRenew from 'components/TokenRenew';
 
-class AppContainer extends PureComponent {
+class App extends PureComponent {
   static propTypes = {
     store: PropTypes.object.isRequired,
   };
@@ -33,4 +33,4 @@ class AppContainer extends PureComponent {
   }
 }
 
-export default AppContainer;
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,24 +5,19 @@ import ReactDOM from 'react-dom';
 import { getBackofficeBrand } from 'config';
 import bootstrap from './bootstrap';
 import createStore from './store/createStore';
-import AppContainer from './containers/AppContainer';
+import AppContainer from './App';
 
 bootstrap();
 
 createStore({}, (store) => {
   const MOUNT_NODE = document.getElementById('root');
 
-  const render = () => {
-    // Check if backoffice brand wasn't found
-    if (!getBackofficeBrand()) {
-      ReactDOM.render(
-        'Brand not found in cookie: brand=BRAND_NAME or in process.env.NAS_BRAND or in local brand configuration',
-        MOUNT_NODE,
-      );
-    } else {
-      ReactDOM.render(<AppContainer store={store} />, MOUNT_NODE);
-    }
-  };
-
-  render();
+  if (!getBackofficeBrand()) {
+    ReactDOM.render(
+      'Brand not found in cookie: brand=BRAND_NAME or in process.env.NAS_BRAND or in local brand configuration',
+      MOUNT_NODE,
+    );
+  } else {
+    ReactDOM.render(<AppContainer store={store} />, MOUNT_NODE);
+  }
 });
