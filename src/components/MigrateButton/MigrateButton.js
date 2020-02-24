0d8645d1fda6216dfa43485a27707264a04b21ab
Sergey Tkachenko <stkachenko@overonix.com>
2020-02-24 09:24:55 +0000

    [FS-508] - [BO] Add Loader when press OK to bulk migrate

--- src/components/MigrateButton/MigrateButton.js
+++ src/components/MigrateButton/MigrateButton.js
@@ -1,22 +1,15 @@
 import React, { PureComponent } from 'react';
-import { withRouter } from 'react-router-dom';
-import { compose } from 'react-apollo';
-import { withRequests } from 'apollo';
-import { get, omit } from 'lodash';
 import I18n from 'i18n-js';
 import PropTypes from 'constants/propTypes';
-import { withNotifications, withModals } from 'components/HighOrder';
-import ConfirmActionModal from 'components/Modal/ConfirmActionModal';
-import clientsBulkMigrationMutation from './graphql/clientsBulkMigrateMutation';
+import { withModals } from 'components/HighOrder';
+import MigrateToFsaModal from 'modals/MigrateToFsaModal';
 import './MigrateButton.scss';
 
 class MigrateButton extends PureComponent {
   static propTypes = {
-    ...PropTypes.router,
     className: PropTypes.string,
-    migrateClientsMutation: PropTypes.func.isRequired,
     modals: PropTypes.shape({
-      confirmationModal: PropTypes.modalType,
+      migrateToFsaModal: PropTypes.modalType,
     }).isRequired,
     variables: PropTypes.shape({
       clients: PropTypes.arrayOf(PropTypes.shape({
@@ -35,55 +28,14 @@ class MigrateButton extends PureComponent {
 
   handleTriggerConfirmationModalToMigrate = () => {
     const {
-      modals: { confirmationModal },
-    } = this.props;
-
-    confirmationModal.show({
-      onSubmit: this.handleSubmitConfirmationModal,
-      modalTitle: I18n.t('MIGRATE.MODAL.TITLE'),
-      actionText: I18n.t('MIGRATE.MODAL.TEXT'),
-      submitButtonLabel: I18n.t('COMMON.OK'),
-    });
-  }
-
-  handleSubmitConfirmationModal = async () => {
-    await this.bulkMigrate();
-    this.props.submitCallback();
-  }
-
-  bulkMigrate = async () => {
-    const {
-      notify,
+      modals: { migrateToFsaModal },
+      submitCallback,
       variables,
-      variables: { allRowsSelected },
-      migrateClientsMutation,
-      location: { query },
-      modals: { confirmationModal },
     } = this.props;
 
-    const response = await migrateClientsMutation({
-      variables: {
-        ...variables,
-        ...(
-          query
-          && allRowsSelected
-          && { searchParams: omit(query.filters, ['page.size']) }
-        ),
-      },
-    });
-
-    const success = get(response, 'data.clients.bulkMigrationUpdate.success') || false;
-
-    confirmationModal.hide();
-
-    notify({
-      level: success ? 'success' : 'error',
-      title: success
-        ? I18n.t('COMMON.SUCCESS')
-        : I18n.t('COMMON.FAIL'),
-      message: success
-        ? I18n.t('MIGRATE.NOTIFICATION.SUCCESS_TEXT')
-        : I18n.t('MIGRATE.NOTIFICATION.ERROR_TEXT'),
+    migrateToFsaModal.show({
+      submitCallback,
+      variables,
     });
   }
 
@@ -102,11 +54,6 @@ class MigrateButton extends PureComponent {
   }
 }
 
-export default compose(
-  withRouter,
-  withNotifications,
-  withModals({ confirmationModal: ConfirmActionModal }),
-  withRequests({
-    migrateClientsMutation: clientsBulkMigrationMutation,
-  }),
-)(MigrateButton);
+export default withModals({
+  migrateToFsaModal: MigrateToFsaModal,
+})(MigrateButton);
