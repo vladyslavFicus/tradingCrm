df0dbaec2ada26b59a1e7a0501371cc74ddf54fa
Artem Polischuk <apolischuk@overonix.com>
2020-04-02 15:29:58 +0000

    Merged in FS-835-handling-dates-of-feeds (pull request #314)
    FS-835 handling dates of feeds
    
    * [FS-828]-[BO] handle values as objects for feeds
    
    * [FS-828]-[BO] removed logs
    
    * [FS-835]-[BO] fixed dates of feeds handling
    
    * [FS-835]-[BO] updated moment library and extended dates of feeds functionality

--- src/components/FeedItem/FeedContent/utils.js
+++ src/components/FeedItem/FeedContent/utils.js
@@ -73,7 +73,7 @@ const isValueMomentValid = (value) => {
 };
 
 const handleDate = (date) => {
-  if (moment(date, 'YYYY-MM-DDTHH:mm:ss.SSS', true).isValid()) {
+  if (moment(date, ['YYYY-MM-DDTHH:mm:ss.SSS', 'YYYY-MM-DDTHH:mm:ss'], true).isValid()) {
     return moment.utc(new Date(date)).local().format('DD.MM.YYYY \\a\\t HH:mm:ss');
   }
 
