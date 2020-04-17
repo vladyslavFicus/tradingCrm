1a1fc4143a006f46ef48967960f8e929cb94843c
Sergey Tkachenko <stkachenko@overonix.com>
2020-04-17 09:30:27 +0000

    Refactoring - removed unused files from src/utils folder

--- src/components/PaymentAccount/PaymentAccount.js
+++ src/components/PaymentAccount/PaymentAccount.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import PropTypes from '../../constants/propTypes';
-import renderPaymentAccount from '../../utils/renderPaymentAccount';
+import React, { PureComponent } from 'react';
+import PropTypes from 'constants/propTypes';
+import { shortify } from 'utils/uuid';
 
-const PaymentAccount = ({ account }) => (
-  <span className="payment-account">
-    {renderPaymentAccount(account)}
-  </span>
-);
-PaymentAccount.propTypes = {
-  account: PropTypes.string.isRequired,
-};
+class PaymentAccount extends PureComponent {
+  static propTypes = {
+    account: PropTypes.string.isRequired,
+  };
+
+  renderPaymentAccount = (account) => {
+    const creditCardMatch = account
+      ? account.match(/^(mc|visa)-(\d{4})/i)
+      : null;
+
+    let formatted = shortify(account, null, 2);
+
+    if (creditCardMatch) {
+      const [, provider, lastNumbers] = creditCardMatch;
+
+      formatted = `${provider} *${lastNumbers}`;
+    }
+
+    return formatted;
+  };
+
+  render() {
+    const { account } = this.props;
+
+    return (
+      <span className="payment-account">
+        {this.renderPaymentAccount(account)}
+      </span>
+    );
+  }
+}
 
 export default PaymentAccount;
--- src/utils/recognizeFieldError.js
+++
@@ -1,7 +0,0 @@
-import I18n from 'i18n-js';
-
-export default function (inputError, map) {
-  const formField = map[inputError.split('.').slice(-1).pop()];
-
-  return formField ? { [formField]: I18n.t(inputError) } : false;
-}
