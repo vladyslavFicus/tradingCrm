34edcb0db8631d74f4dc342aa9aa55620e47c36e
Yevhen Savitskyi <ysavitskyi@easytech.co>
2020-05-15 15:28:30 +0300

    [FS-203] - removed promoteLeadProfile query from LeadProfile

--- src/routes/Leads/routes/LeadProfile/graphql/index.js
+++ src/routes/Leads/routes/LeadProfile/graphql/index.js
@@ -1,6 +1,5 @@
 import NotesQuery from './NotesQuery';
 import LeadProfileQuery from './LeadProfileQuery';
-import PromoteLeadProfile from './PromoteLeadProfile';
 import AddLeadProfileNote from './AddLeadProfileNote';
 import RemoveLeadProfileNote from './RemoveLeadProfileNote';
 import UpdateLeadProfileNote from './UpdateLeadProfileNote';
@@ -8,7 +7,6 @@ import UpdateLeadProfileNote from './UpdateLeadProfileNote';
 export {
   NotesQuery,
   LeadProfileQuery,
-  PromoteLeadProfile,
   AddLeadProfileNote,
   RemoveLeadProfileNote,
   UpdateLeadProfileNote,
