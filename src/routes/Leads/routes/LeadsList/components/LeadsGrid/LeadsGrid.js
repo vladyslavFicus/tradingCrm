ebdff38dfeaa438077c532489eeaf3c334776646
Sergey Tkachenko <stkachenko@overonix.com>
2020-06-03 11:20:02 +0300

    [FS-1139] - added some fixes based on comments to pull-request

--- src/routes/Leads/routes/LeadsList/components/LeadsGrid/LeadsGrid.js
+++ src/routes/Leads/routes/LeadsList/components/LeadsGrid/LeadsGrid.js
@@ -80,7 +80,7 @@ class LeadsGrid extends PureComponent {
         modals: { confirmationModal },
       } = this.props;
 
-      const { totalElements } = get(leadsData, 'data.leads.data') || {};
+      const totalElements = get(leadsData, 'data.leads.data.totalElements');
       const searchLimit = get(location, 'query.filters.size') || null;
 
       const selectedLimit = (searchLimit && searchLimit < totalElements)
