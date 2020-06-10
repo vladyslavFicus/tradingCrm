c02ec47279c71f2afcbb69a627231696599259ff
Valery Zakharchenko <vzakharchenko@newagesol.com>
2020-06-10 16:12:57 +0300

    fix: added docs to Queue.js

--- src/apollo/links/AuthLink/Queue.js
+++ src/apollo/links/AuthLink/Queue.js
@@ -1,10 +1,20 @@
 import { setHeaders } from './utils';
 
+/**
+ * Queue with unauthorized operations
+ */
 class Queue {
   constructor() {
     this.queue = [];
   }
 
+  /**
+   * Add operation to queue
+   *
+   * @param operation
+   * @param forward
+   * @param observer
+   */
   push({ operation, forward, observer }) {
     const request = {
       operation,
@@ -15,16 +25,26 @@ class Queue {
     this.queue.push(request);
   }
 
+  /**
+   * Consume queue
+   *
+   * @param token New refreshed token
+   */
   consume(token) {
     this.queue.forEach(({ forward, operation, observer }) => {
+      // Set new refreshed token to operation
       setHeaders(operation, { authorization: `Bearer ${token}` });
 
+      // Retry operation
       forward(operation).subscribe(observer);
     });
 
     this.queue = [];
   }
 
+  /**
+   * Clean queue
+   */
   clean() {
     this.queue = [];
   }
