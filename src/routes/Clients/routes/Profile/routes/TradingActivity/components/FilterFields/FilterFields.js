95b8f37be7aea84ac7c2ad398215592037fb50fe
Yevhen Savitskyi <ysavitskyi@easytech.co>
2020-06-18 18:43:22 +0300

    [FS-1431] - refactored filters

---
+++ src/routes/Clients/routes/Profile/routes/TradingActivity/components/FilterFields/FilterFields.js
@@ -0,0 +1,271 @@
+import React, { PureComponent } from 'react';
+import I18n from 'i18n-js';
+import { get } from 'lodash';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'react-apollo';
+import { Formik, Form, Field } from 'formik';
+import { withRequests } from 'apollo';
+import PropTypes from 'constants/propTypes';
+import { accountTypes } from 'constants/accountTypes';
+import { statuses as operatorsStasuses } from 'constants/operators';
+import {
+  FormikInputField,
+  FormikSelectField,
+  FormikDateRangeGroup,
+} from 'components/Formik';
+import { RangeGroup } from 'components/Forms';
+import { Button } from 'components/UI';
+import PlatformTypeBadge from 'components/PlatformTypeBadge';
+import {
+  types,
+  symbols,
+  statuses,
+} from '../../attributes/constants';
+import {
+  TradingAccountsQuery,
+  OperatorsQuery,
+} from './graphql';
+
+class FilterForm extends PureComponent {
+  static propTypes = {
+    ...PropTypes.router,
+    tradingAccountsQuery: PropTypes.query({
+      tradingAccount: PropTypes.array, // ?
+    }).isRequired,
+    operatorsQuery: PropTypes.query({
+      operators: PropTypes.shape({
+        data: PropTypes.pageable(PropTypes.tradingActivityOriginalAgent),
+        error: PropTypes.object,
+      }),
+    }).isRequired,
+  };
+
+  componentWillUnmount() {
+    this.handleFilterReset();
+  }
+
+  handleApplyFilters = (values) => {
+    this.props.history.replace({
+      query: {
+        filters: {
+          ...values,
+          ...values.tradeId && { tradeId: Number(values.tradeId) },
+          ...values.volumeFrom && { volumeFrom: Number(values.volumeFrom) },
+          ...values.volumeTo && { volumeTo: Number(values.volumeTo) },
+        },
+      },
+    });
+  };
+
+  handleFilterReset = () => {
+    this.props.history.replace({
+      query: { filters: {} },
+    });
+  };
+
+  render() {
+    const {
+      operatorsQuery: {
+        data: operatorsData,
+        loading: operatorsLoading,
+      },
+      tradingAccountsQuery: {
+        data: tradingAccountsData,
+        loading: tradingAccountsLoading,
+      },
+    } = this.props;
+
+    const accounts = get(tradingAccountsData, 'tradingAccount') || [];
+    const originalAgents = get(operatorsData, 'operators.data.content') || [];
+    const disabledOriginalAgentField = get(operatorsData, 'operators.error') || operatorsLoading;
+
+    return (
+      <Formik
+        initialValues={{ tradeType: 'LIVE' }}
+        onSubmit={this.handleApplyFilters}
+        onReset={this.handleFilterReset}
+      >
+        {({ handleReset, dirty }) => (
+          <Form className="filter__form">
+            <div className="filter__form-inputs">
+              <Field
+                name="tradeId"
+                label={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.TRADE_LABEL')}
+                placeholder={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.TRADE_PLACEHOLDER')}
+                className="filter-row__big"
+                component={FormikInputField}
+                disabled={tradingAccountsLoading}
+              />
+              <Field
+                name="loginIds"
+                label={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.LOGIN_IDS')}
+                placeholder={I18n.t('COMMON.SELECT_OPTION.ALL')}
+                className="filter-row__medium"
+                component={FormikSelectField}
+                disabled={tradingAccountsLoading}
+                multiple
+              >
+                {accounts.map(({ login, platformType }) => (
+                  <option key={login} value={login}>
+                    <PlatformTypeBadge platformType={platformType} center>
+                      {login}
+                    </PlatformTypeBadge>
+                  </option>
+                ))}
+              </Field>
+              <Field
+                name="operationType"
+                label={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.TYPE_LABEL')}
+                placeholder={I18n.t('COMMON.SELECT_OPTION.ANY')}
+                className="filter-row__medium"
+                component={FormikSelectField}
+                disabled={tradingAccountsLoading}
+                withAnyOption
+                searchable
+              >
+                {types.map(({ value, label }) => (
+                  <option key={value} value={value}>
+                    {I18n.t(label)}
+                  </option>
+                ))}
+              </Field>
+              <Field
+                name="symbol"
+                label={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.SYMBOL_LABEL')}
+                placeholder={I18n.t('COMMON.SELECT_OPTION.ANY')}
+                className="filter-row__medium"
+                component={FormikSelectField}
+                disabled={tradingAccountsLoading}
+                withAnyOption
+                searchable
+              >
+                {symbols.map(({ value, label }) => (
+                  <option key={value} value={value}>
+                    {I18n.t(label)}
+                  </option>
+                ))}
+              </Field>
+              <Field
+                name="agentIds"
+                label={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.ORIGINAL_AGENT_LABEL')}
+                placeholder={I18n.t('COMMON.SELECT_OPTION.ANY')}
+                className="filter-row__medium"
+                component={FormikSelectField}
+                disabled={tradingAccountsLoading || disabledOriginalAgentField}
+                searchable
+                multiple
+              >
+                {originalAgents.map(({ fullName, uuid, operatorStatus }) => (
+                  <option
+                    key={uuid}
+                    value={uuid}
+                    className={operatorStatus === operatorsStasuses.INACTIVE
+                      || operatorStatus === operatorsStasuses.CLOSE
+                      ? 'color-inactive'
+                      : ''
+                    }
+                  >
+                    {fullName}
+                  </option>
+                ))}
+              </Field>
+              <RangeGroup
+                className="form-group filter-row__medium"
+                label={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.VOLUME_LABEL')}
+              >
+                <Field
+                  name="volumeFrom"
+                  type="number"
+                  step="0.01"
+                  min={0}
+                  placeholder="0"
+                  component={FormikInputField}
+                  disabled={tradingAccountsLoading}
+                />
+                <Field
+                  name="volumeTo"
+                  type="number"
+                  step="0.01"
+                  min={0}
+                  placeholder="0"
+                  component={FormikInputField}
+                  disabled={tradingAccountsLoading}
+                />
+              </RangeGroup>
+              <Field
+                name="status"
+                label={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.STATUS_LABEL')}
+                placeholder={I18n.t('COMMON.SELECT_OPTION.ANY')}
+                className="filter-row__medium"
+                component={FormikSelectField}
+                disabled={tradingAccountsLoading}
+                withAnyOption
+              >
+                {statuses.map(({ value, label }) => (
+                  <option key={value} value={value}>
+                    {I18n.t(label)}
+                  </option>
+                ))}
+              </Field>
+              <Field
+                name="tradeType"
+                label={I18n.t('CONSTANTS.TRANSACTIONS.FILTER_FORM.ATTRIBUTES_LABELS.ACCOUNT_TYPE')}
+                placeholder={I18n.t('COMMON.SELECT_OPTION.ANY')}
+                className="filter-row__medium"
+                component={FormikSelectField}
+                disabled={tradingAccountsLoading}
+                withAnyOption
+              >
+                {accountTypes.map(({ value, label }) => (
+                  <option key={value} value={value}>
+                    {I18n.t(label)}
+                  </option>
+                ))}
+              </Field>
+              <FormikDateRangeGroup
+                className="form-group filter-row__date-range"
+                label={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.OPEN_TIME_RANGE_LABEL')}
+                periodKeys={{
+                  start: 'openTimeStart',
+                  end: 'openTimeEnd',
+                }}
+              />
+              <FormikDateRangeGroup
+                className="form-group filter-row__date-range"
+                label={I18n.t('CLIENT_PROFILE.TRADING_ACTIVITY.FILTER_FORM.CLOSE_TIME_RANGE_LABEL')}
+                periodKeys={{
+                  start: 'closeTimeStart',
+                  end: 'closeTimeEnd',
+                }}
+              />
+            </div>
+            <div className="filter__form-buttons">
+              <Button
+                className="margin-right-15"
+                onClick={handleReset}
+                disabled={!dirty}
+                common
+              >
+                {I18n.t('COMMON.RESET')}
+              </Button>
+              <Button
+                type="submit"
+                primary
+              >
+                {I18n.t('COMMON.APPLY')}
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    );
+  }
+}
+
+export default compose(
+  withRouter,
+  withRequests({
+    tradingAccountsQuery: TradingAccountsQuery,
+    operatorsQuery: OperatorsQuery,
+  }),
+)(FilterForm);
--- src/routes/Clients/routes/Profile/routes/TradingActivity/components/FilterFields/graphql/TradingAccountsQuery.js
+++ src/routes/Clients/routes/Profile/routes/TradingActivity/components/FilterFields/graphql/TradingAccountsQuery.js
@@ -6,31 +6,8 @@ import { Query } from 'react-apollo';
 const REQUEST = gql`
   query TradingAccountsQuery($uuid: String!) {
     tradingAccount(uuid: $uuid) {
-      accountUUID
-      currency
-      balance
-      credit
-      margin
-      name
       login
-      group
-      accountType
       platformType
-      archived
-      leverage
-      readOnlyUpdateTime
-      readOnlyUpdatedBy
-      readOnly
-      profileUUID
-      operator {
-        fullName
-      }
-      lastLeverageChangeRequest {
-        changeLeverageFrom
-        changeLeverageTo
-        status
-        createDate
-      }
     }
   }
 `;
