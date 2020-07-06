6bd824a05831048dc530008c68240f651ccb5378
Sergey Tkachenko <stkachenko@overonix.com>
2020-07-06 18:20:02 +0300

    [FS-1544] - fixed crach of client mini profile if the operator have no access to view client data

--- src/components/MiniProfile/PlayerMiniProfile/PlayerMiniProfile.js
+++ src/components/MiniProfile/PlayerMiniProfile/PlayerMiniProfile.js
@@ -24,6 +24,16 @@ const PlayerMiniProfile = ({ miniProfile: { data, loading } }) => {
     );
   }
 
+  if (data.newProfile?.error?.error === 'error.profile.access.hierarchy.not-subordinate') {
+    return (
+      <div className="mini-profile-error">
+        <div className="mini-profile-error-message">
+          {I18n.t('MINI_PROFILE.NO_ACCESS.CLIENT')}
+        </div>
+      </div>
+    );
+  }
+
   const {
     newProfile: {
       data: {
