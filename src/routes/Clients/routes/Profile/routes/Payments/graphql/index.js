2d53b635af190475f72514856bc5b986b4201e56
Igor Sorokopud <igorsorokopud@ip-192-168-1-191.eu-central-1.compute.internal>
2020-09-23 15:31:58 +0300

    Reverted changes

--- src/routes/Clients/routes/Profile/routes/Payments/graphql/index.js
+++ src/routes/Clients/routes/Profile/routes/Payments/graphql/index.js
@@ -1,9 +1,7 @@
 import PaymentsQuery from './PaymentsQuery';
 import ProfileQuery from './ProfileQuery';
-import PartnersQuery from './PartnersQuery';
 
 export {
   PaymentsQuery,
   ProfileQuery,
-  PartnersQuery,
 };
