ca26284f28c0f8bd0d8e16d44d6829ba52c080ba
Valery Zakharchenko <vzakharchenko@newagesol.com>
2020-11-06 17:44:04 +0200

    [FS-2435] - next part of permissions added to rbac

--- src/constants/rbac.js
+++ src/constants/rbac.js
@@ -5,60 +5,60 @@ export default [
   {
     id: 'leads',
     permissions: [
+      // List of leads
       {
         id: 'list',
         actions: {
-          // List of leads
           view: {
             action: 'lead.searchLeads',
             state: false,
           },
         },
       },
+      // Lead's profile
       {
         id: 'profile',
         actions: {
-          // Lead's profile
           view: {
             action: 'lead.getLeadById',
             state: false,
           },
         },
       },
+      // Promote lead to client
       {
         id: 'promote',
         actions: {
-          // Promote lead to client
           edit: {
             action: 'profile.admin.createProfile',
             state: false,
           },
         },
       },
+      // Phone
       {
         id: 'phone',
         actions: {
-          // Phone
           view: {
             action: 'backoffice-graphql.lead.field.phone',
             state: false,
           },
         },
       },
+      // Mobile
       {
         id: 'mobile',
         actions: {
-          // Mobile
           view: {
             action: 'backoffice-graphql.lead.field.mobile',
             state: false,
           },
         },
       },
+      // Email
       {
         id: 'email',
         actions: {
-          // Email
           view: {
             action: 'backoffice-graphql.lead.field.email',
             state: false,
@@ -73,140 +73,140 @@ export default [
   {
     id: 'operators',
     permissions: [
+      // Create new operator
       {
         id: 'create',
         actions: {
-          // Create new operator
           edit: {
             action: 'operator.createOperator',
             state: false,
           },
         },
       },
+      // Operator's profile
       {
         id: 'profile',
         actions: {
-          // Operator's profile
           view: {
             action: 'operator.getOperatorProfileByUuid',
             state: false,
           },
         },
       },
+      // List of operators
       {
         id: 'list',
         actions: {
-          // List of operators
           view: {
             action: 'operator.searchOperators',
             state: false,
           },
         },
       },
+      // Add authority for user
       {
         id: 'addAuthority',
         actions: {
-          // Add authority for user
           edit: {
             action: 'auth2.addUserAuthority',
             state: false,
           },
         },
       },
+      // Delete authority for user
       {
         id: 'deleteAuthority',
         actions: {
-          // Delete authority for user
           edit: {
             action: 'auth2.deleteUserAuthority',
             state: false,
           },
         },
       },
+      // Operator's personal details
       {
         id: 'personalDetails',
         actions: {
-          // Operator's personal details
           edit: {
             action: 'operator.editOperatorProfile',
             state: false,
           },
         },
       },
+      // Reset password for user
       {
         id: 'resetPassword',
         actions: {
-          // Reset password for user
           edit: {
             action: 'auth2.user.resetPassword',
             state: false,
           },
         },
       },
+      // Operator's status
       {
         id: 'changeStatus',
         actions: {
-          // Operator's status
           edit: {
             action: 'operator.changeOperatorStatus',
             state: false,
           },
         },
       },
+      // Change password for other operator
       {
         id: 'changePassword',
         actions: {
-          // Change password for other operator
           edit: {
             action: 'auth2.operator.changePassword',
             state: false,
           },
         },
       },
+      // Get authorities on edit profile
       {
         id: 'getAuthorities',
         actions: {
-          // Get authorities on edit profile
           view: {
             action: 'auth2.getAuthorities',
             state: false,
           },
         },
       },
+      // List of operators
       {
         id: 'list2',
         actions: {
-          // List of Operators
           view: {
             action: 'hierarchy.user.getSubordinateOperators',
             state: false,
           },
         },
       },
+      // User branch
       {
         id: 'updateBranch',
         actions: {
-          // User branch
           edit: {
             action: 'hierarchy-updater.user.updateUserBranch',
             state: false,
           },
         },
       },
+      // Create sales rule
       {
         id: 'createSalesRule',
         actions: {
-          // Create sales rule
           edit: {
             action: 'rules-profile.createOrUpdateRule',
             state: false,
           },
         },
       },
+      // Delete sales rule
       {
         id: 'deleteSalesRule',
         actions: {
-          // Delete sales rule
           edit: {
             action: 'rules-profile.deleteRule',
             state: false,
@@ -221,102 +221,466 @@ export default [
   {
     id: 'partners',
     permissions: [
+      // List of partners
       {
         id: 'list',
         actions: {
-          // List of partners
           view: {
             action: 'affiliate.searchAffiliate',
             state: false,
           },
         },
       },
+      // Partner's profile
       {
         id: 'profile',
         actions: {
-          // Partner's profile
           view: {
             action: 'affiliate.getAffiliate',
             state: false,
           },
         },
       },
+      // Create new partner
       {
         id: 'create',
         actions: {
-          // Create new partner
           edit: {
             action: 'affiliate.createAffiliate',
             state: false,
           },
         },
       },
+      // Partner account status
       {
         id: 'changeStatus',
         actions: {
-          // Partner account status
           edit: {
             action: 'affiliate.changeAffiliateStatus',
             state: false,
           },
         },
       },
+      // Partner personal details
       {
         id: 'personalDetails',
         actions: {
-          // Partner personal details
           edit: {
             action: 'affiliate.updateAffiliate',
             state: false,
           },
         },
       },
+      // Change password for partner
       {
         id: 'changePassword',
         actions: {
-          // Change password for partner
           edit: {
             action: 'auth2.operator.changePassword',
             state: false,
           },
         },
       },
+      // Show feed tab
       {
         id: 'feedTab',
         actions: {
-          // Show feed tab
           view: {
             action: 'audit.searchAudit',
             state: false,
           },
         },
       },
+      // List of sales rules
       {
         id: 'listSalesRules',
         actions: {
-          // List of sales rules
           view: {
             action: 'rules-profile.searchRules',
             state: false,
           },
         },
       },
+      // Create sales rule
       {
         id: 'createSalesRule',
         actions: {
-          // Create sales rule
           edit: {
             action: 'rules-profile.createOrUpdateRule',
             state: false,
           },
         },
       },
+      // Delete sales rule
       {
         id: 'deleteSalesRule',
         actions: {
-          // Delete sales rule
           edit: {
             action: 'rules-profile.deleteRule',
+            state: false,
+          },
+        },
+      },
+    ],
+  },
+  // ============================================= //
+  // ========== Management - Hierarchy =========== //
+  // ============================================= //
+  {
+    id: 'management-hierarchy',
+    permissions: [
+      // Show hierarchy tree
+      {
+        id: 'tree',
+        actions: {
+          view: {
+            action: 'hierarchy.branch.getBranchTree',
+            state: false,
+          },
+        },
+      },
+      // List of Offices
+      {
+        id: 'listOffices',
+        actions: {
+          view: {
+            action: 'hierarchy.branch.searchOffice',
+            state: false,
+          },
+        },
+      },
+      // List of Desks
+      {
+        id: 'listDesks',
+        actions: {
+          view: {
+            action: 'hierarchy.branch.searchDesk',
+            state: false,
+          },
+        },
+      },
+      // List of Teams
+      {
+        id: 'listTeams',
+        actions: {
+          view: {
+            action: 'hierarchy.branch.searchTeam',
+            state: false,
+          },
+        },
+      },
+      // List of Operators
+      {
+        id: 'listOperators',
+        actions: {
+          view: {
+            action: 'hierarchy.user.getSubordinateOperators',
+            state: false,
+          },
+        },
+      },
+      // Branch page
+      {
+        id: 'branchPage',
+        actions: {
+          view: {
+            action: 'hierarchy.branch.getBranchByUuid',
+            state: false,
+          },
+        },
+      },
+      // Create new branch
+      {
+        id: 'branchCreate',
+        actions: {
+          edit: {
+            action: 'hierarchy-updater.branch.createBranch',
+            state: false,
+          },
+        },
+      },
+      // Add branch manager
+      {
+        id: 'branchAddManager',
+        actions: {
+          edit: {
+            action: 'hierarchy-updater.branch.addBranchManager',
+            state: false,
+          },
+        },
+      },
+      // Remove branch manager
+      {
+        id: 'removeAddManager',
+        actions: {
+          edit: {
+            action: 'hierarchy-updater.branch.removeBranchManager',
+            state: false,
+          },
+        },
+      },
+    ],
+  },
+  // ============================================= //
+  // ========= Management - Sales Rules ========== //
+  // ============================================= //
+  {
+    id: 'management-sales-rules',
+    permissions: [
+      // List of sales rules
+      {
+        id: 'salesRulesList',
+        actions: {
+          view: {
+            action: 'rules-profile.searchRules',
+            state: false,
+          },
+        },
+      },
+      // Create sales rule
+      {
+        id: 'salesRulesCreate',
+        actions: {
+          edit: {
+            action: 'rules-profile.createOrUpdateRule',
+            state: false,
+          },
+        },
+      },
+      // Delete sales rule
+      {
+        id: 'salesRulesDelete',
+        actions: {
+          edit: {
+            action: 'rules-profile.deleteRule',
+            state: false,
+          },
+        },
+      },
+    ],
+  },
+  // ============================================= //
+  // ================== Payments ================= //
+  // ============================================= //
+  {
+    id: 'payments',
+    permissions: [
+      // List of payments
+      {
+        id: 'list',
+        actions: {
+          view: {
+            action: 'payment.searchPayments',
+            state: false,
+          },
+        },
+      },
+      // Approve payment
+      {
+        id: 'approve',
+        actions: {
+          edit: {
+            action: 'payment.approvePayment',
+            state: false,
+          },
+        },
+      },
+      // Reject payment
+      {
+        id: 'reject',
+        actions: {
+          edit: {
+            action: 'payment.rejectPayment',
+            state: false,
+          },
+        },
+      },
+      // Change payment status
+      {
+        id: 'changeStatus',
+        actions: {
+          edit: {
+            action: 'payment.changePaymentStatus',
+            state: false,
+          },
+        },
+      },
+      // Change payment method
+      {
+        id: 'changeMethod',
+        actions: {
+          edit: {
+            action: 'payment.changePaymentMethod',
+            state: false,
+          },
+        },
+      },
+      // Payment original agent
+      {
+        id: 'changeAgent',
+        actions: {
+          edit: {
+            action: 'payment.changePaymentAgent',
+            state: false,
+          },
+        },
+      },
+    ],
+  },
+  // ============================================= //
+  // ================== Callbacks ================ //
+  // ============================================= //
+  {
+    id: 'callbacks',
+    permissions: [
+      // List of callbacks
+      {
+        id: 'list',
+        actions: {
+          view: {
+            action: 'callback.searchCallbacks',
+            state: false,
+          },
+        },
+      },
+    ],
+  },
+  // ============================================= //
+  // ============== Email templates ============== //
+  // ============================================= //
+  {
+    id: 'email-templates',
+    permissions: [
+      // List of templates
+      {
+        id: 'list',
+        actions: {
+          view: {
+            action: 'email.getAllTemplates',
+            state: false,
+          },
+        },
+      },
+      // Create template
+      {
+        id: 'create',
+        actions: {
+          edit: {
+            action: 'email.createTemplate',
+            state: false,
+          },
+        },
+      },
+      // Send email
+      {
+        id: 'send',
+        actions: {
+          edit: {
+            action: 'email.sendTemplatedEmail',
+            state: false,
+          },
+        },
+      },
+    ],
+  },
+  // ============================================= //
+  // ================ KYC documents ============== //
+  // ============================================= //
+  {
+    id: 'kyc-documents',
+    permissions: [
+      // List of KYC documents
+      {
+        id: 'list',
+        actions: {
+          view: {
+            action: 'attachments.searchAttachments',
+            state: false,
+          },
+        },
+      },
+    ],
+  },
+  // ============================================= //
+  // =============== Trading accounts ============ //
+  // ============================================= //
+  {
+    id: 'trading-accounts',
+    permissions: [
+      // List of trading accounts
+      {
+        id: 'list',
+        actions: {
+          view: {
+            action: 'accountview.search',
+            state: false,
+          },
+        },
+      },
+      // Create trading account
+      {
+        id: 'create',
+        actions: {
+          edit: {
+            action: 'trading-account.createAccount',
+            state: false,
+          },
+        },
+      },
+      // Trading account password
+      {
+        id: 'changePassword',
+        actions: {
+          edit: {
+            action: 'trading-account.changeAccountPassword',
+            state: false,
+          },
+        },
+      },
+    ],
+  },
+  // ============================================= //
+  // ==================== Notes ================== //
+  // ============================================= //
+  {
+    id: 'notes',
+    permissions: [
+      // Notes tab
+      {
+        id: 'list',
+        actions: {
+          view: {
+            action: 'note.searchNotesPost',
+            state: false,
+          },
+        },
+      },
+      // Create note
+      {
+        id: 'create',
+        actions: {
+          edit: {
+            action: 'note.saveNote',
+            state: false,
+          },
+        },
+      },
+      // Update note
+      {
+        id: 'update',
+        actions: {
+          edit: {
+            action: 'note.updateNote',
+            state: false,
+          },
+        },
+      },
+      // Delete note
+      {
+        id: 'delete',
+        actions: {
+          edit: {
+            action: 'note.deleteNote',
             state: false,
           },
         },
