21b84280e84d327e8ae36e4c99620e4b21e3599d
Sergey Tkachenko <stkachenko@overonix.com>
2020-11-23 16:48:59 +0200

    [FS-2793] - extended ErrorBoundler to show error message that is not appear in browser console

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import I18n from 'i18n-js';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'constants/propTypes';
+import './ErrorBoundary.scss';
 
 class ErrorBoundary extends PureComponent {
   static propTypes = {
@@ -9,30 +10,52 @@ class ErrorBoundary extends PureComponent {
     children: PropTypes.element.isRequired,
   };
 
-  static getDerivedStateFromError() {
-    return { hasError: true };
+  static getDerivedStateFromError(error) {
+    return {
+      hasError: true,
+      error,
+    };
   }
 
   static getDerivedStateFromProps({ location: { pathname } }, state) {
     if (pathname !== state.pathname) {
       return {
         hasError: false,
+        error: null,
         pathname,
       };
     }
     return null;
   }
 
+  componentDidCatch(error) {
+    this.setState({ error: error.stack });
+  }
+
   state = {
     hasError: false,
     pathname: null,
+    error: null,
   };
 
   render() {
+    const { hasError, error } = this.state;
+
+    console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
     return (
       <Choose>
-        <When condition={this.state.hasError}>
-          <h1 dangerouslySetInnerHTML={{ __html: I18n.t('COMMON.ERROR_CONTENT') }} />;
+        <When condition={hasError}>
+          <div className="ErrorBoundary">
+            <div className="ErrorBoundary__container">
+              <h1 className="ErrorBoundary__title">{I18n.t('COMMON.ERROR_TITLE')}</h1>
+              <p className="ErrorBoundary__description">{I18n.t('COMMON.ERROR_CONTENT')}</p>
+
+              <If condition={error && process.env.NODE_ENV === 'development'}>
+                <div className="ErrorBoundary__error" dangerouslySetInnerHTML={{ __html: error }} />
+              </If>
+            </div>
+          </div>
         </When>
         <Otherwise>
           {this.props.children}
