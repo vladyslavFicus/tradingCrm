4353d02538debb5aabf1f506b5d0b73b7140abad
Yevhen Savitskyi <ysavitskyi@easytech.co>
2020-11-30 17:18:18 +0200

    [FS-2720] - removed legacy RuleModal and EditRuleModal

--- src/components/HierarchyProfileRules/components/EditRuleModal/graphql/index.js
+++
@@ -1,9 +0,0 @@
-import OperatorsQuery from './OperatorsQuery';
-import PartnersQuery from './PartnersQuery';
-import GetRulesQuery from './GetRulesQuery';
-
-export {
-  OperatorsQuery,
-  PartnersQuery,
-  GetRulesQuery,
-};
--- src/components/HierarchyProfileRules/components/RuleModal/graphql/index.js
+++
@@ -1,3 +0,0 @@
-export { default as OperatorsQuery } from './OperatorsQuery';
-export { default as PartnersQuery } from './PartnersQuery';
-export { default as RulesQuery } from './RulesQuery';
