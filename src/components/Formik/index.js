e8f71d98e7f4c43cb3044f11ed51e35ddaa0aba0
Sergey Tkachenko <stkachenko@overonix.com>
2021-01-14 14:34:36 +0200

    [FS-3157] - switched all FormikDatePicker to new one, removed old ones, remove unecessary components and libs

--- src/components/Formik/index.js
+++ src/components/Formik/index.js
@@ -4,7 +4,6 @@ import FormikSelectField from './FormikSelectField';
 import FormikTextEditorField from './FormikTextEditorField';
 import FormikDateRangePicker from './FormikDateRangePicker';
 import FormikDatePicker from './FormikDatePicker';
-import FormikDatePickerNew from './FormikDatePickerNew';
 import FormikMultiInputField from './FormikMultiInputField';
 import FormikSwitchField from './FormikSwitchField';
 import FormikTextAreaField from './FormikTextAreaField';
@@ -15,7 +14,6 @@ export {
   FormikSelectField,
   FormikTextEditorField,
   FormikDatePicker,
-  FormikDatePickerNew,
   FormikDateRangePicker,
   FormikMultiInputField,
   FormikSwitchField,
