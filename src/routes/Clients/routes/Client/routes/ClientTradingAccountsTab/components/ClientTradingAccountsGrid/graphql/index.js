c35f8cd64e5d6d7c99693d832788aeb9cb4b8ede
Yevhen Savitskyi <ysavitskyi@easytech.co>
2021-01-25 17:43:28 +0200

    [FS-2889] - recovery of right mutations names in ClientTradingAccountsGrid

--- src/routes/Clients/routes/Client/routes/ClientTradingAccountsTab/components/ClientTradingAccountsGrid/graphql/index.js
+++ src/routes/Clients/routes/Client/routes/ClientTradingAccountsTab/components/ClientTradingAccountsGrid/graphql/index.js
@@ -1,3 +1,3 @@
-export { default as UpdateTradingAccountMutation } from './U';
-export { default as ApproveChangingLeverageMutation } from './A';
-export { default as RejectChangingLeverageMutation } from './R';
+export { default as UpdateTradingAccountMutation } from './UpdateTradingAccountMutation';
+export { default as ApproveChangingLeverageMutation } from './ApproveChangingLeverageMutation';
+export { default as RejectChangingLeverageMutation } from './RejectChangingLeverageMutation';
