da741da3b8b2a86f1efcb3af2ad0573184647230
Igor Sorokopud <igorsorokopud@ip-192-168-1-187.eu-central-1.compute.internal>
2021-07-06 17:54:29 +0300

    [TRAD-68] - Added permissions for trading engine url

--- src/routes/TradingEngine/TradingEngine/TradingEngine.js
+++ src/routes/TradingEngine/TradingEngine/TradingEngine.js
@@ -2,6 +2,9 @@ import React, { PureComponent, Suspense } from 'react';
 import { Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'constants/propTypes';
 import Route from 'components/Route';
+import permissions from 'config/permissions';
+import { withPermission } from 'providers/PermissionsProvider';
+import NotFound from 'routes/NotFound';
 import TradingEngineAccountsGrid from '../components/TradingEngineAccountsGrid';
 import TradingEngineOrdersGrid from '../components/TradingEngineOrdersGrid';
 import TradingEngineSymbolsGrid from '../components/TradingEngineSymbolsGrid';
@@ -14,29 +17,38 @@ class TradingEngine extends PureComponent {
       path: PropTypes.string,
       url: PropTypes.string,
     }).isRequired,
+    permission: PropTypes.permission.isRequired,
   }
 
   render() {
     const {
       match: { path, url },
+      permission,
     } = this.props;
 
     return (
-      <div className="TradingEngine">
-        <div className="Client__tab-content">
-          <Suspense fallback={null}>
-            <Switch>
-              <Route exact path={`${path}/accounts`} component={TradingEngineAccountsGrid} />
-              <Route path={`${path}/accounts/:id`} component={TradingEngineAccountProfile} />
-              <Route path={`${path}/orders`} component={TradingEngineOrdersGrid} />
-              <Route path={`${path}/symbols`} component={TradingEngineSymbolsGrid} />
-              <Redirect to={`${url}/accounts`} />
-            </Switch>
-          </Suspense>
-        </div>
-      </div>
+      <Choose>
+        <When condition={permission.allows(permissions.TRADING_ENGINE.GET_ACCOUNTS)}>
+          <div className="TradingEngine">
+            <div className="Client__tab-content">
+              <Suspense fallback={null}>
+                <Switch>
+                  <Route exact path={`${path}/accounts`} component={TradingEngineAccountsGrid} />
+                  <Route path={`${path}/accounts/:id`} component={TradingEngineAccountProfile} />
+                  <Route path={`${path}/orders`} component={TradingEngineOrdersGrid} />
+                  <Route path={`${path}/symbols`} component={TradingEngineSymbolsGrid} />
+                  <Redirect to={`${url}/accounts`} />
+                </Switch>
+              </Suspense>
+            </div>
+          </div>
+        </When>
+        <Otherwise>
+          <NotFound />
+        </Otherwise>
+      </Choose>
     );
   }
 }
 
-export default TradingEngine;
+export default withPermission(TradingEngine);
