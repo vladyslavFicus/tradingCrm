7f4db8738a88c6d1523e00e2c5909fcc90f9b6d0
Serhii Ostapenko <sostapenko@easytech.co>
2021-07-27 09:15:50 +0300

    fix showPhone button

--- src/routes/Clients/routes/Client/components/ShowClientPhoneButton/ShowClientPhoneButton.js
+++ src/routes/Clients/routes/Client/components/ShowClientPhoneButton/ShowClientPhoneButton.js
@@ -2,35 +2,31 @@ import React, { PureComponent } from 'react';
 import I18n from 'i18n-js';
 import PropTypes from 'constants/propTypes';
 import { Button } from 'components/UI';
-import { withPermission } from 'providers/PermissionsProvider';
 import permissions from 'config/permissions';
 import './ShowClientPhoneButton.scss';
+import PermissionContent from 'components/PermissionContent';
 
 class ShowClientPhoneButton extends PureComponent {
   static propTypes = {
-    permission: PropTypes.permission.isRequired,
     onClick: PropTypes.func.isRequired,
   };
 
   render() {
     const {
-      permission: { allows },
       onClick,
     } = this.props;
 
-    const isAvailable = allows(permissions.USER_PROFILE.FIELD_PHONE);
-
     return (
-      <If condition={isAvailable}>
+      <PermissionContent permissions={permissions.USER_PROFILE.FIELD_PHONE}>
         <Button
           className="ShowClientPhone__button"
           onClick={onClick}
         >
           {I18n.t('PLAYER_PROFILE.PROFILE.CONTACTS.SHOW')}
         </Button>
-      </If>
+      </PermissionContent>
     );
   }
 }
 
-export default withPermission(ShowClientPhoneButton);
+export default ShowClientPhoneButton;
