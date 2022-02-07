02b7b1164394f6ddafc46b55c4ad5ba105920b0c
Valery Zakharchenko <vzakharchenko@newagesol.com>
2022-02-07 14:03:04 +0200

    feat: eslint graphql rules

---
+++ .eslintrc.js
@@ -0,0 +1,153 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:jsx-control-statements/recommended',
+  ],
+  plugins: [
+    'jsx-control-statements',
+    'graphql',
+  ],
+  overrides: [
+    // Typescript linting
+    {
+      parser: '@typescript-eslint/parser',
+      files: ['*.ts', '*.tsx'],
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+      ],
+      rules: {
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'import/no-unresolved': [2, { commonjs: true, amd: true }],
+        'import/named': 2,
+        'import/namespace': 2,
+        'import/default': 2,
+        'import/export': 2,
+      },
+    },
+  ],
+  rules: {
+    'graphql/template-strings': ['error', {
+      env: 'apollo',
+      schemaJson: require('./graphql.schema.json'),
+      validators: [
+        'ExecutableDefinitionsRule',
+        'FieldsOnCorrectTypeRule',
+        'FragmentsOnCompositeTypesRule',
+        'KnownArgumentNamesRule',
+        'KnownTypeNamesRule',
+        'LoneAnonymousOperationRule',
+        'NoFragmentCyclesRule',
+        // 'NoUndefinedVariablesRule',
+        'NoUnusedVariablesRule',
+        'OverlappingFieldsCanBeMergedRule',
+        'PossibleFragmentSpreadsRule',
+        'ProvidedRequiredArgumentsRule',
+        'ScalarLeafsRule',
+        'SingleFieldSubscriptionsRule',
+        'UniqueArgumentNamesRule',
+        'UniqueDirectivesPerLocationRule',
+        'UniqueFragmentNamesRule',
+        'UniqueInputFieldNamesRule',
+        'UniqueOperationNamesRule',
+        'UniqueVariableNamesRule',
+        'ValuesOfCorrectTypeRule',
+        'VariablesAreInputTypesRule',
+        'VariablesInAllowedPositionRule',
+      ],
+    }],
+    // START: Workaround for babel-eslint https://github.com/babel/babel-eslint/issues/799
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        VariableDeclarator: 1,
+        outerIIFEBody: 1,
+        FunctionDeclaration: {
+          parameters: 1,
+          body: 1,
+        },
+        FunctionExpression: {
+          parameters: 1,
+          body: 1,
+        },
+        CallExpression: {
+          arguments: 1,
+        },
+        ArrayExpression: 1,
+        ObjectExpression: 1,
+        ImportDeclaration: 1,
+        flatTernaryExpressions: false,
+        ignoredNodes: ['TemplateLiteral', 'JSXElement', 'JSXElement > *', 'JSXAttribute', 'JSXIdentifier', 'JSXNamespacedName', 'JSXMemberExpression', 'JSXSpreadAttribute', 'JSXExpressionContainer', 'JSXOpeningElement', 'JSXClosingElement', 'JSXFragment', 'JSXOpeningFragment', 'JSXClosingFragment', 'JSXText', 'JSXEmptyExpression', 'JSXSpreadChild'],
+        ignoreComments: false,
+      },
+    ],
+    'template-curly-spacing': 'off',
+    // END: Workaround
+
+    // Fix error on importing .ts|.tsx files
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        cjs: 'never',
+      },
+    ],
+
+    'max-len': [2, 120],
+    'react/prefer-stateless-function': 0,
+    'react/forbid-foreign-prop-types': 0,
+    'react/jsx-filename-extension': 0,
+    'react/destructuring-assignment': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'jsx-a11y/label-has-for': 0,
+    'no-underscore-dangle': 0,
+    'react/no-array-index-key': 0,
+    'react/forbid-prop-types': 0,
+    'object-curly-newline': 0,
+    'class-methods-use-this': 0,
+    'react/no-danger': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/mouse-events-have-key-events': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-no-undef': [2, { allowGlobals: true }],
+    'react/jsx-one-expression-per-line': 0,
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'react/sort-comp': 0,
+    'import/no-cycle': 0,
+  },
+  globals: {
+    window: false,
+    document: false,
+    fetch: false,
+    ExtractApolloTypeFromArray: false,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: 'src',
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
