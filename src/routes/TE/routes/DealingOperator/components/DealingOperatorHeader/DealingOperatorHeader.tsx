5e05194d861266f390ae3a4293c69b69279d1194
Yevhen Marchenko <ymarchenko@easytech.co>
2022-03-23 15:38:19 +0000

    Merged in FS-4502-dealing-operator-profile-reset-pass (pull request #1039)
    FS-4506 [FE] Reset password on Dealing operator profile
    
    * update PR
    
    * fix PR notes
    
    * Merge branch 'master' into FS-4502-dealing-operator-profile
    
    * fix PR
    
    * Merge branch 'master' into FS-4502-dealing-operator-profile
    
    # Conflicts:
    #	src/routes/TE/TE.tsx
    
    * Merge branch 'FS-4502-dealing-operator-profile' into FS-4502-dealing-operator-profile-reset-pass
    
    # Conflicts:
    #	src/routes/TE/routes/DealingOperator/components/DealingOperatorHeader/DealingOperatorHeader.tsx
    
    * fix error
    
    * fix operator type
    
    * Merge branch 'FS-4502-dealing-operator-profile' into FS-4502-dealing-operator-profile-reset-pass
    
    # Conflicts:
    #	src/routes/TE/routes/DealingOperator/components/DealingOperatorHeader/DealingOperatorHeader.tsx
    
    * Merge branch 'master' into FS-4502-dealing-operator-profile-reset-pass
    
    # Conflicts:
    #	src/i18n/en.json
    #	src/routes/TE/routes/DealingOperator/components/DealingOperatorHeader/DealingOperatorHeader.tsx
    
    
    Approved-by: Valery Zakharchenko

--- src/routes/TE/routes/DealingOperator/components/DealingOperatorHeader/DealingOperatorHeader.tsx
+++ src/routes/TE/routes/DealingOperator/components/DealingOperatorHeader/DealingOperatorHeader.tsx
@@ -1,18 +1,126 @@
 import React from 'react';
 import I18n from 'i18n-js';
+import compose from 'compose-function';
+import { parseErrors } from 'apollo';
+import { LevelType, Modal, Notify } from 'types';
+import { withModals, withNotifications } from 'hoc';
+import { passwordMaxSize, passwordPattern } from 'routes/TE/constants';
 import permissions from 'config/permissions';
+import EventEmitter, { OPERATOR_RELOAD } from 'utils/EventEmitter';
 import PermissionContent from 'components/PermissionContent';
 import { Button } from 'components/UI';
 import Uuid from 'components/Uuid';
+import ChangePasswordModal from 'modals/ChangePasswordModal';
+import ConfirmActionModal from 'modals/ConfirmActionModal';
 import { Operator } from '../../DealingOperator';
+import { useChangeOperatorPasswordMutation } from './graphql/__generated__/ChangeOperatorPasswordMutation';
+import { useResetOperatorPasswordMutation } from './graphql/__generated__/ResetOperatorPasswordMutation';
+import { useUnlockOperatorLoginMutation } from './graphql/__generated__/UnlockOperatorLoginMutation';
 import './DealingOperatorHeader.scss';
 
 type Props = {
   operator: Operator,
-}
+  notify: Notify,
+  modals: {
+    changePasswordModal: Modal,
+    confirmActionModal: Modal,
+  }
+};
 
-const DealingOperatorHeader = ({ operator }: Props) => {
+const DealingOperatorHeader = (props: Props) => {
+  const { operator, notify, modals: { changePasswordModal, confirmActionModal } } = props;
   const { status, firstName, lastName, uuid } = operator;
+  const [changePasswordMutation] = useChangeOperatorPasswordMutation();
+  const [resetPasswordMutation] = useResetOperatorPasswordMutation();
+  const [unlockOperatorLoginMutation] = useUnlockOperatorLoginMutation();
+
+  const handleChangePassword = async ({ newPassword }: any) => {
+    try {
+      await changePasswordMutation({ variables: { uuid, newPassword } });
+
+      notify({
+        level: LevelType.SUCCESS,
+        title: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.CHANGE_PASSWORD.SUCCESS.TITLE'),
+        message: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.CHANGE_PASSWORD.SUCCESS.MESSAGE'),
+      });
+
+      changePasswordModal.hide();
+    } catch (e) {
+      const error = parseErrors(e);
+
+      notify({
+        level: LevelType.ERROR,
+        title: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.CHANGE_PASSWORD.ERROR.TITLE'),
+        message: I18n.t(
+          error.error,
+          { defaultValue: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.CHANGE_PASSWORD.ERROR.MESSAGE') },
+        ),
+      });
+    }
+  };
+
+  const handleResetPassword = async () => {
+    try {
+      await resetPasswordMutation({ variables: { uuid } });
+
+      notify({
+        level: LevelType.SUCCESS,
+        title: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.RESET_PASSWORD.SUCCESS.TITLE'),
+        message: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.RESET_PASSWORD.SUCCESS.MESSAGE'),
+      });
+
+      confirmActionModal.hide();
+    } catch (e) {
+      const error = parseErrors(e);
+
+      notify({
+        level: LevelType.ERROR,
+        title: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.RESET_PASSWORD.ERROR.TITLE'),
+        message: I18n.t(
+          error.error,
+          { defaultValue: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.RESET_PASSWORD.ERROR.MESSAGE') },
+        ),
+      });
+    }
+  };
+
+  const handleUnlockOperatorLogin = async () => {
+    try {
+      await unlockOperatorLoginMutation({ variables: { uuid } });
+
+      notify({
+        level: LevelType.SUCCESS,
+        title: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.UNLOCK.SUCCESS.TITLE'),
+        message: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.UNLOCK.SUCCESS.MESSAGE'),
+      });
+
+      EventEmitter.emit(OPERATOR_RELOAD);
+    } catch (e) {
+      notify({
+        level: LevelType.ERROR,
+        title: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.UNLOCK.ERROR.TITLE'),
+        message: I18n.t('TRADING_ENGINE.OPERATOR_PROFILE.NOTIFICATIONS.UNLOCK.ERROR.MESSAGE'),
+      });
+    }
+  };
+
+  const handleResetAction = () => confirmActionModal.show({
+    uuid,
+    onSubmit: handleResetPassword,
+    fullName: `${firstName} ${lastName}`,
+    modalTitle: I18n.t('PLAYER_PROFILE.PROFILE.RESET_PASSWORD_MODAL.TITLE'),
+    actionText: I18n.t('PLAYER_PROFILE.PROFILE.RESET_PASSWORD_MODAL.TEXT'),
+    submitButtonLabel: I18n.t('PLAYER_PROFILE.PROFILE.RESET_PASSWORD_MODAL.BUTTON_ACTION'),
+  });
+
+  const handleChangedAction = () => changePasswordModal.show({
+    uuid,
+    fullName: `${firstName} ${lastName}`,
+    passwordPattern,
+    passwordMaxSize,
+    passwordCustomError: I18n.t('COMMON.OPERATOR_PASSWORD_INVALID'),
+    onSubmit: handleChangePassword,
+  });
 
   return (
     <div className="DealingOperatorHeader">
@@ -28,7 +136,7 @@ const DealingOperatorHeader = ({ operator }: Props) => {
           <If condition={status !== 'CLOSED'}>
             <Button
               className="DealingOperatorHeader__action"
-              onClick={() => console.log('not implemented')}
+              onClick={handleUnlockOperatorLogin}
               primary
               small
             >
@@ -41,7 +149,7 @@ const DealingOperatorHeader = ({ operator }: Props) => {
           <PermissionContent permissions={permissions.WE_TRADING.OPERATORS_RESET_PASSWORD}>
             <Button
               className="DealingOperatorHeader__action"
-              onClick={() => console.log('not implemented')}
+              onClick={handleResetAction}
               primary
               small
             >
@@ -53,7 +161,7 @@ const DealingOperatorHeader = ({ operator }: Props) => {
         <PermissionContent permissions={permissions.WE_TRADING.OPERATORS_CHANGE_PASSWORD}>
           <Button
             className="DealingOperatorHeader__action"
-            onClick={() => console.log('not implemented')}
+            onClick={handleChangedAction}
             primary
             small
           >
@@ -65,4 +173,11 @@ const DealingOperatorHeader = ({ operator }: Props) => {
   );
 };
 
-export default React.memo(DealingOperatorHeader);
+export default compose(
+  React.memo,
+  withNotifications,
+  withModals({
+    changePasswordModal: ChangePasswordModal,
+    confirmActionModal: ConfirmActionModal,
+  }),
+)(DealingOperatorHeader);
