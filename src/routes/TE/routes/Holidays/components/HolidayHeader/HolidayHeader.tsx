61fb2cabfa7245d7e0ab8082eaeaa6eb73dd0ed2
Valery Zakharchenko <vzakharchenko@newagesol.com>
2022-03-28 19:10:41 +0300

    fix: remove unused code

--- src/routes/TE/routes/Holidays/components/HolidayHeader/HolidayHeader.tsx
+++ src/routes/TE/routes/Holidays/components/HolidayHeader/HolidayHeader.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import I18n from 'i18n-js';
 import { FormikProps } from 'formik';
-import ReactPlaceholder from 'react-placeholder';
-import { TextRow } from 'react-placeholder/lib/placeholders';
 import { Button } from 'components/UI';
 import { FormValues } from '../../types';
 import './HolidayHeader.scss';
@@ -17,28 +15,16 @@ const HolidayHeader = ({ formik }: Props) => {
 
   return (
     <div className="HolidayHeader">
-      <ReactPlaceholder
-        ready={date !== undefined}
-        customPlaceholder={(
-          <div>
-            <TextRow
-              className="animated-background"
-              style={{ width: '220px', height: '20px' }}
-            />
-          </div>
-        )}
-      >
-        <div className="HolidayHeader__title">
-          <Choose>
-            <When condition={!!date}>
-              {date}
-            </When>
-            <Otherwise>
-              {I18n.t('TRADING_ENGINE.HOLIDAY.NEW_HOLIDAY')}
-            </Otherwise>
-          </Choose>
-        </div>
-      </ReactPlaceholder>
+      <div className="HolidayHeader__title">
+        <Choose>
+          <When condition={!!date}>
+            {date}
+          </When>
+          <Otherwise>
+            {I18n.t('TRADING_ENGINE.HOLIDAY.NEW_HOLIDAY')}
+          </Otherwise>
+        </Choose>
+      </div>
       <Button
         small
         primary
