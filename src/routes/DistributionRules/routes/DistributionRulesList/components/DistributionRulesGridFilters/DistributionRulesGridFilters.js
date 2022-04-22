13f2e7c57989674234dc58c4ae26aafa43ba987f
Valery Zakharchenko <vzakharchenko@easytech.co>
2022-04-22 14:22:29 +0000

    Merged in FS-4880-rename-field-execution-periods-in-houres (pull request #1087)
    [FS-4880] - rename field execution periods in hours
    
    * [FS-4880] - rename field execution periods in hours

--- src/routes/DistributionRules/routes/DistributionRulesList/components/DistributionRulesGridFilters/DistributionRulesGridFilters.js
+++ src/routes/DistributionRules/routes/DistributionRulesList/components/DistributionRulesGridFilters/DistributionRulesGridFilters.js
@@ -194,7 +194,7 @@ class DistributionRulesFilters extends PureComponent {
                   withFocus
                 />
                 <Field
-                  name="executionPeriodsInHours"
+                  name="timesInCurrentStatusInHours"
                   className="DistributionRulesFilters__field DistributionRulesFilters__select"
                   placeholder={I18n.t('COMMON.SELECT_OPTION.ANY')}
                   label={I18n.t('CLIENTS_DISTRIBUTION.FILTERS.TIME_IN_STATUS')}
