3e8af07f74974ae03d106dd6eeca273b93612e2a
Valery Zakharchenko <vzakharchenko@easytech.co>
2022-06-21 09:55:40 +0000

    Merged in FS-5325-hide-checkbox-on-symbol-overrides (pull request #1143)
    [FS-5325] - Hide enabled checkbox on symbols overrides
    
    * [FS-5325] - Hide enabled checkbox on symbols overrides

--- src/routes/TE/routes/Groups/modals/GroupNewSymbolModal/GroupNewSymbolModal.tsx
+++ src/routes/TE/routes/Groups/modals/GroupNewSymbolModal/GroupNewSymbolModal.tsx
@@ -166,7 +166,7 @@ const GroupNewSymbolModal = ({
                     name="enabled"
                     component={FormikCheckbox}
                     label={I18n.t('TRADING_ENGINE.GROUP.COMMON_GROUP_FORM.ENABLE')}
-                    className="GroupNewSymbolModal__field  GroupNewSymbolModal__field--center"
+                    className="GroupNewSymbolModal__field  GroupNewSymbolModal__field--hide"
                     disabled={baseSymbols.includes(values.symbol)}
                   />
                   <div className="GroupNewSymbolModal__fields">
