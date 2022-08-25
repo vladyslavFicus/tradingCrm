ea3e83ddd924a59fb4a338ab762da9354da9cd68
Tiko Papikyan <tpapikyan@easytech.co>
2022-08-25 13:03:56 +0000

    Merged in FS-5560-non-nullabe-values (pull request #1221)
    FS-5754   [FE] Fix country for outdated TAs
    
    * [FS-5560] fixed non nullabe values
    
    * Fixed Commets
    
    * fix flag component typing error
    
    
    Approved-by: Yevhen Marchenko

--- src/routes/TE/routes/AccountProfile/components/AccountProfileCountry/AccountProfileCountry.tsx
+++ src/routes/TE/routes/AccountProfile/components/AccountProfileCountry/AccountProfileCountry.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import I18n from 'i18n-js';
+import Flag from 'react-country-flag';
 import countryList, { getCountryCode } from 'utils/countryList';
 import { Account } from '../../AccountProfile';
 import './AccountProfileCountry.scss';
@@ -14,7 +15,22 @@ const AccountProfileCountry = ({ account }: Props) => (
       {I18n.t('TRADING_ENGINE.ACCOUNT_PROFILE.COUNTRY')}
     </div>
     <div className="AccountProfileCountry__primary">
-      {countryList[getCountryCode(account.country)]}
+      <Choose>
+        <When condition={getCountryCode(account.country)}>
+          <Flag
+            svg
+            style={{
+              height: 10,
+            }}
+            countryCode={getCountryCode(account.country)}
+          />
+          {' '}
+          {countryList[getCountryCode(account.country)]}
+        </When>
+        <Otherwise>
+          <img src="/img/unknown-country-flag.svg" alt="" />
+        </Otherwise>
+      </Choose>
     </div>
   </div>
 );
