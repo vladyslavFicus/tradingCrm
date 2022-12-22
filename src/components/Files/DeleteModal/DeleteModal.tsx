c5192190132057bc0ebc32e62a07a358d4a5e1a3
Bura Tetiana <tbura@easytech.co>
2022-12-22 15:02:01 +0000

    Merged in FS-6478-fe-attachments-don-t-allow-to-press-delete-attac (pull request #1351)
    [FS-6478] - Attachments. Don't allow to press Delete attachments button several times
    
    * [FS-6478] - Attachments. Don't allow to press Delete attachments button several times
    
    * [FS-6478] - Attachments. Don't allow to press Delete attachments button several times
    
    * Merge branch 'master' of bitbucket.org:easytech-global/backoffice into FS-6478-fe-attachments-don-t-allow-to-press-delete-attac
    
    * updated
    
    * updated
    
    
    Approved-by: Michail Kalugin
    Approved-by: Tkachyk Volodymyr

---
+++ src/components/Files/DeleteModal/DeleteModal.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import I18n from 'i18n-js';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { File } from '__generated__/types';
+import { LevelType, notify } from 'providers/NotificationProvider';
+import EventEmitter, { FILE_REMOVED } from 'utils/EventEmitter';
+import { Button } from 'components/UI';
+import { useDeleteFileMutation } from './graphql/__generated__/DeleteFileMutation';
+import './DeleteModal.scss';
+
+type Props = {
+  file: File,
+  onCloseModal: () => void,
+}
+
+const DeleteModal = (props: Props) => {
+  const { file, onCloseModal } = props;
+
+  const [deleteFileMutation, { loading }] = useDeleteFileMutation();
+
+  const handleDelete = async () => {
+    try {
+      await deleteFileMutation({ variables: { uuid: file.uuid } });
+
+      EventEmitter.emit(FILE_REMOVED, file);
+
+      notify({
+        level: LevelType.SUCCESS,
+        title: I18n.t('FILES.DELETE_MODAL.NOTIFICATIONS.TITLE'),
+        message: I18n.t('FILES.DELETE_MODAL.NOTIFICATIONS.SUCCESS'),
+      });
+
+      onCloseModal();
+    } catch {
+      notify({
+        level: LevelType.ERROR,
+        title: I18n.t('FILES.DELETE_MODAL.NOTIFICATIONS.TITLE'),
+        message: I18n.t('FILES.DELETE_MODAL.NOTIFICATIONS.ERROR'),
+      });
+    }
+  };
+
+  return (
+    <Modal className="DeleteModal" toggle={onCloseModal} isOpen>
+      <ModalHeader toggle={onCloseModal} className="DeleteModal__header">
+        {I18n.t('FILES.DELETE_MODAL.TITLE')}
+      </ModalHeader>
+
+      <ModalBody>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: I18n.t('FILES.DELETE_MODAL.ACTION_TEXT', {
+              fileName: file.fileName,
+            }),
+          }}
+        />
+        <div className="DeleteModal__warning"> {I18n.t('FILES.DELETE_MODAL.WARNING_TEXT')} </div>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          tertiary
+          onClick={onCloseModal}
+        >
+          {I18n.t('COMMON.BUTTONS.CANCEL')}
+        </Button>
+
+        <Button
+          danger
+          disabled={loading}
+          onClick={handleDelete}
+        >
+          {I18n.t('FILES.DELETE_MODAL.BUTTONS.DELETE')}
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default React.memo(DeleteModal);
