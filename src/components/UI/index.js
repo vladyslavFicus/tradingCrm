707aa7aff1ac34962b5a2e4484023df8cbce01d4
Bura Tetiana <tbura@easytech.co>
2022-12-23 11:19:53 +0000

    Merged in FS-6583--fe-rewrite-component-removebutton-from-js-to-ts (pull request #1381)
    [FS-6583] - Rewrite component RemoveButton from JS to TS
    
    * [FS-6583] - Rewrite component RemoveButton from JS to TS
    
    
    Approved-by: Oleksii Morhun
    Approved-by: Michail Kalugin

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,6 +1,5 @@
 export { default as Button } from './Button';
 export { default as EditButton } from './EditButton';
-export { default as RemoveButton } from './RemoveButton';
 export { default as RefreshButton } from './RefreshButton';
 export { default as DownloadButton } from './DownloadButton';
 export { default as TrashButton } from './TrashButton';
