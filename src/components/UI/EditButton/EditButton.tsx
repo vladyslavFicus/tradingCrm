12cecd1af474daa7335e3bd07ed97fa4ba670a3c
Bura Tetiana <tbura@easytech.co>
2023-01-04 14:05:57 +0000

    Merged in FS-6581--rewrite-component-editbutton-from-js-to-ts (pull request #1377)
    [FS-6581] - Rewrite component EditButton from JS to TS
    
    * [FS-6581] - Rewrite component EditButton from JS to TS
    
    * updated
    
    
    Approved-by: Mikhailo Kalugin
    Approved-by: Oleksii Morhun

---
+++ src/components/UI/EditButton/EditButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Button } from 'components/UI';
+import { ReactComponent as EditIcon } from './icon-edit.svg';
+import './EditButton.scss';
+
+const EditButton = (props: Omit<React.ComponentProps<typeof Button>, 'children'>) => {
+  const { className = '', disabled, ...rest } = props;
+
+  return (
+    <Button
+      {...rest}
+      icon
+      disabled={disabled}
+      className={classNames('EditButton', { 'EditButton--disabled': disabled }, className)}
+    >
+      <EditIcon className="EditButton__icon" />
+    </Button>
+  );
+};
+
+export default React.memo(EditButton);
