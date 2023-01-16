669e5de84825f5803195c8097e18a76ac4f8c30c
Bura Tetiana <btetiana@crmdata.info>
2023-01-16 08:17:04 +0000

    Merged in FS-6591--fe-rewrite-component-releasenotes-from-js-to-ts (pull request #1404)
    [FS-6591] - Rewrite component ReleaseNotes from JS to TS
    
    * [FS-6591] - Rewrite component ReleaseNotes from JS to TS
    
    
    Approved-by: Mikhailo Kalugin
    Approved-by: Oleksii Morhun
    Approved-by: Tkachyk Volodymyr

---
+++ src/routes/ReleaseNotes/ReleaseNotes.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import { startCase } from 'lodash';
+import { getBackofficeBrand } from 'config';
+import ShortLoader from 'components/ShortLoader';
+import './ReleaseNotes.scss';
+
+const ReleaseNotes = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [content, setContent] = useState<string>('');
+
+  useEffect(() => {
+    fetch('/cloud-static/RELEASE-NOTES.html')
+      .then(response => response.text())
+      .then((data) => {
+        setContent(data);
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Currently assumed that this condition will run only once
+    // so there isn't any flag for restriction of event handlers initialization
+    if (!loading && content) {
+      document.querySelectorAll('.ReleaseNotes__item').forEach((item) => {
+        const headline: HTMLHeadingElement | null = item.querySelector('.ReleaseNotes__item-headline');
+        const itemBody: HTMLElement | null = item.querySelector('.ReleaseNotes__item-content');
+
+        if (headline && itemBody) {
+          headline.onclick = () => {
+            if (itemBody.style.maxHeight) {
+              itemBody.style.maxHeight = '0';
+            } else {
+              itemBody.style.maxHeight = `${itemBody.scrollHeight}px`;
+            }
+          };
+        }
+      });
+    }
+  }, [loading, content]);
+
+  return (
+    <div className="ReleaseNotes">
+      <h1 className="ReleaseNotes__headline">
+        {startCase(getBackofficeBrand().id)} CRM<br />Release notes
+      </h1>
+
+      <Choose>
+        <When condition={loading}>
+          <ShortLoader />
+        </When>
+
+        <Otherwise>
+          <div
+            className="ReleaseNotes__content"
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+        </Otherwise>
+      </Choose>
+    </div>
+  );
+};
+
+export default React.memo(ReleaseNotes);
